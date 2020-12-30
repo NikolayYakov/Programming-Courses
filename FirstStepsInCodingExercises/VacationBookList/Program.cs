1a5cf870e248e64ac0fd7220e0a3b4e06bc42d5a
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2020-12-30 15:38:03 +0200

    Create Program.cs

---
+++ FirstStepsInCodingExercises/VacationBookList/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Dynamic;
+
+namespace VacationBookList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sheets = int.Parse(Console.ReadLine());
+            double sheetsPerDay = double.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            double hoursNeeded = sheets / sheetsPerDay;
+            double hoursADayNeeded = hoursNeeded / days;
+            Console.WriteLine(hoursADayNeeded);
+        }
+    }
+}
