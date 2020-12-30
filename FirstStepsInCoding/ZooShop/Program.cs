f7501782a28b66130170d902386bb1fe34132e02
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2020-12-30 15:37:20 +0200

    Create Program.cs

---
+++ FirstStepsInCoding/ZooShop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ZooShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogs = int.Parse(Console.ReadLine());
+            int animals = int.Parse(Console.ReadLine());
+            double PriceFoodDogs = dogs * 2.50;
+            int PriceFoodAnimals = animals * 4;
+            double FullPrice = PriceFoodDogs + PriceFoodAnimals;
+
+            Console.WriteLine($"{FullPrice} lv.");
+        }
+    }
+}
