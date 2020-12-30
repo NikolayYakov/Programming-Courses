368fec6827ce8bd00339bf92fc1a500c6316f604
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2020-12-30 15:37:59 +0200

    Create Program.cs

---
+++ FirstStepsInCodingExercises/RadToGrad/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RadToGrad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rad = double.Parse(Console.ReadLine());
+            double grad = rad * 180 / Math.PI;
+            Console.WriteLine(Math.Round(grad));
+        }
+    }
+}
