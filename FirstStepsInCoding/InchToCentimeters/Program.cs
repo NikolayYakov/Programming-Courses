e55ac66cea10496b9baf50009080df05bae6eea6
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2020-12-30 15:37:03 +0200

    Create Program.cs

---
+++ FirstStepsInCoding/InchToCentimeters/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InchToCentimeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Inch = double.Parse(Console.ReadLine());
+            double Centimeters = Inch * 2.54;
+            Console.WriteLine(Centimeters);
+        }
+    }
+}
