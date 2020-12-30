1f9524ec4cb4a4b5d520f3c653cb7a038e258d35
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2020-12-30 15:36:54 +0200

    Create Program.cs

---
+++ FirstStepsInCoding/GreatingByName/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GreatingByName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = Console.ReadLine();
+            Console.WriteLine($"Hello, {Name}! ");
+        }
+    }
+}
