3a4d6a2bd2110e3966b6bb410bc61b696091d8cf
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2020-12-31 12:07:22 +0200

    Create Program.cs

---
+++ ProgramingBasics/ChecksExercises/BonusScore/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BonusScore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double score = double.Parse(Console.ReadLine());
+             double bonusPoints = 0.0;
+            double firstScore = score - bonusPoints;
+            if (score <= 100)
+            {
+                bonusPoints += 5;
+                score += bonusPoints;
+            }
+            else if (score > 1000)
+            {
+                bonusPoints = score * 10 / 100;
+                score += bonusPoints;
+            }
+            else
+            {
+                bonusPoints = score * 20 / 100;
+                score += bonusPoints;
+            }
+
+            if (firstScore % 2 == 0)
+            {
+                bonusPoints= bonusPoints+1;
+                score +=1;
+            }
+            else if (firstScore % 5 == 0)
+            {
+                bonusPoints=bonusPoints+2;
+                score += 2;
+            }
+            Console.WriteLine(bonusPoints);
+            Console.WriteLine(score);
+        }
+    }
+}
