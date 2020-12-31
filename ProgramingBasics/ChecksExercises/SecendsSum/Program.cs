12ded5e0abc9c86340d54232f829f329ed24a684
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2020-12-31 12:07:39 +0200

    Create Program.cs

---
+++ ProgramingBasics/ChecksExercises/SecendsSum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SecendsSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstTime = int.Parse(Console.ReadLine());
+            int secondTime = int.Parse(Console.ReadLine());
+            int thirdTime = int.Parse(Console.ReadLine());
+            int totalTime = firstTime + secondTime + thirdTime;
+            if (totalTime < 60)
+            {
+                if (totalTime >= 10)
+                {
+
+                    Console.WriteLine($"0:{totalTime}");
+
+                }
+                else
+                {
+                    Console.WriteLine($"0:0{totalTime}");
+                }
+            }
+            else
+            {
+                int minutes = totalTime / 60;
+                int seconds = totalTime % 60;
+                if (seconds < 10)
+                {
+                    Console.WriteLine($"{minutes}:0{seconds:f0}");
+                }
+                else
+                {
+                    Console.WriteLine($"{minutes}:{seconds}");
+                }
+            }
+        }
+    }
+}
