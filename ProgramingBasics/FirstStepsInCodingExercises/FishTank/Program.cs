9a02629035298e44c1dd35640b77dfacbbb26446
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2020-12-31 12:09:16 +0200

    Create Program.cs

---
+++ ProgramingBasics/FirstStepsInCodingExercises/FishTank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FishTank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lengthCm = int.Parse(Console.ReadLine());
+            int widthCm = int.Parse(Console.ReadLine());
+            int heightCm = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+            double fishTankVolume = lengthCm * widthCm * heightCm;
+            double liters = fishTankVolume / 1000;
+            double finalPercent = percent / 100;
+            double neededliters = liters * (1 - finalPercent);
+            Console.WriteLine(neededliters);
+        }
+    }
+}
