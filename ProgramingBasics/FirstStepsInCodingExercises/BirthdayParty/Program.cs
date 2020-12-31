ecbc6ab33ff0e6a7d7b64747b7fe7fe76741b7f0
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2020-12-31 12:09:02 +0200

    Create Program.cs

---
+++ ProgramingBasics/FirstStepsInCodingExercises/BirthdayParty/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BirthdayParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double hallRent = double.Parse(Console.ReadLine());
+            double cake = hallRent * 20 / 100;
+            double drinks = cake * 55 / 100;
+            double animator = hallRent * 1 / 3;
+            double fullPrice = hallRent + cake + drinks + animator;
+            Console.WriteLine(fullPrice);
+        }
+    }
+}
