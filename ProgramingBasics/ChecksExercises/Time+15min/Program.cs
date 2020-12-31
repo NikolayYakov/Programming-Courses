02a5975cfafd6374458098ba24c754b6fd644800
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2020-12-31 12:07:51 +0200

    Create Program.cs

---
+++ ProgramingBasics/ChecksExercises/Time+15min/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Time_15min
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            minutes += 15;
+            if (hours > 23)
+            {
+                hours = 0;
+            }
+            if (minutes < 60)
+            {
+                if (minutes >= 10)
+                {
+                    Console.WriteLine($"{hours}:{minutes}");
+                }
+                else
+                {
+                    Console.WriteLine($"{hours}:0{minutes}");
+                }
+            }
+            else
+            {
+                hours += 1;
+                minutes -= 60;
+                if (hours > 23)
+                {
+                    hours = 0;
+                }
+                if (minutes >= 10)
+                {
+                    Console.WriteLine($"{hours}:{minutes}");
+                }
+                else
+                {
+                    Console.WriteLine($"{hours}:0{minutes}");
+                }
+            }
+
+        }
+    }
+}
