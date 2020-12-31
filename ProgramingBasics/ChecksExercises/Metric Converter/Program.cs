7d80dd7b18b414b61e87495e8630f16e5bda47bb
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2020-12-31 12:07:33 +0200

    Create Program.cs

---
+++ ProgramingBasics/ChecksExercises/Metric Converter/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Metric_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            string inputUnit = Console.ReadLine();
+            string outputUnit = Console.ReadLine();
+            
+             
+            if (inputUnit == "cm")
+            {
+                if(outputUnit=="m")
+                {
+                    number /= 100;
+                }
+                else
+                {
+                    number *= 10;
+                }
+            }
+            else if (inputUnit == "m")
+            {
+                if (outputUnit == "cm")
+                {
+                    number *= 100;
+                }
+                else
+                {
+                    number *= 1000;
+                }
+            }
+            else
+            {
+                if (outputUnit == "cm")
+                {
+                    number /= 10;
+                }
+                else
+                {
+                    number /= 1000;
+                }
+            }
+            Console.WriteLine($"{number:f3}");
+        }
+    }
+}
