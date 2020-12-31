b074ba2cdf01d366786707414f29b310e7c0b4ec
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2020-12-31 12:06:49 +0200

    Create Program.cs

---
+++ ProgramingBasics/Checks/EvenOrOdd/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EvenOrOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int evenOrOdd = number % 2;
+            bool finalCheck = evenOrOdd == 0;
+            if (finalCheck == true)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
