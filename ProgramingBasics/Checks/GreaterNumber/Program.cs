6c8cd1c297f72cb82bc63bc78247558629572231
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2020-12-31 12:06:58 +0200

    Create Program.cs

---
+++ ProgramingBasics/Checks/GreaterNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GreaterNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberA = int.Parse(Console.ReadLine());
+            int numberB = int.Parse(Console.ReadLine());
+            bool greaterNumber = numberA > numberB;
+            if(greaterNumber == true)
+            {
+                Console.WriteLine(numberA);
+            }
+            else
+            {
+                Console.WriteLine(numberB);
+            }
+        }
+    }
+}
