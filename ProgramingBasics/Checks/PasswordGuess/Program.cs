1632e1cb27dd70530193fa10a19635a308fa79e6
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2020-12-31 12:07:08 +0200

    Create Program.cs

---
+++ ProgramingBasics/Checks/PasswordGuess/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PasswordGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            bool passwordCheck = password == "s3cr3t!P@ssw0rd";
+            if (passwordCheck == true)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
