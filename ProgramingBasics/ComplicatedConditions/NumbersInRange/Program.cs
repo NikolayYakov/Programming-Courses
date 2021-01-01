15850d0b5da88507a91c688820cc751e2d69ff42
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-01 14:31:46 +0200

    Create Program.cs

---
+++ ProgramingBasics/ComplicatedConditions/NumbersInRange/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace NumbersInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            if (number <= 100)
+            {
+                if(number >= -100)
+                {
+                    if (number != 0)
+                    {
+                        Console.WriteLine("Yes");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
