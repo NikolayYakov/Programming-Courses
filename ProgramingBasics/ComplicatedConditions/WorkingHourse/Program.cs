902d462f51d6fab0b6fa6e980fd62113d0314bee
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-01 14:32:30 +0200

    Create Program.cs

---
+++ ProgramingBasics/ComplicatedConditions/WorkingHourse/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WorkingHourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            String day = Console.ReadLine();
+            if (day == "Sunday")
+            {
+                Console.WriteLine("closed");
+            }
+            else
+            {
+                if (hour < 10)
+                {
+                    Console.WriteLine("closed");
+                }
+                else if (hour > 18)
+                {
+                    Console.WriteLine("closed");
+                }
+                else
+                {
+                    Console.WriteLine("open");
+                }
+            }
+        }
+    }
+}
+ 
