30f6ca422244f8e2e485987a7edfb0faddd81588
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-01 14:32:18 +0200

    Create Program.cs

---
+++ ProgramingBasics/ComplicatedConditions/ValidNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ValidNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (number < 100)
+            {
+                if (number != 0)
+                {
+                    Console.WriteLine("invalid");
+                }
+            }
+            if (number > 200)
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
