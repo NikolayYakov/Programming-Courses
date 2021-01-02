838b19c2380df37dfd54a4f6654003d43cbbb86a
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-02 15:03:37 +0200

    Create Program.cs

---
+++ ProgramingBasics/For-Loop-Exercise/FIne/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FIne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tabsOpen = int.Parse(Console.ReadLine());
+            double salary =int.Parse(Console.ReadLine());
+            for(int i = 1; i <= tabsOpen; i++)
+            {
+                string tabs = Console.ReadLine();
+                if(tabs=="Facebook")
+                {
+                    salary -= 150.0;
+                }
+               if(tabs=="Instagram")
+                {
+                    salary -= 100.0;
+                }
+               if(tabs=="Reddit")
+                {
+                    salary -= 50.0;
+                }
+                if (salary <= 0)
+                {
+                    Console.WriteLine("You have lost your salary.");
+                     
+                    break;
+                }
+                 
+            }
+            if (salary > 0)
+            {
+                Console.WriteLine($"{salary:f0}");
+            }
+
+        }
+    }
+}
