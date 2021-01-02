cada0c36cf75d7bf6da8daa1c5ac0de54663ed93
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-02 15:04:11 +0200

    Create Program.cs

---
+++ ProgramingBasics/For-Loop/Even/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Even
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= number; i += 1)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(Math.Pow(2, i));
+                }
+            }
+        }
+    }
+}
