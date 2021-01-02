c1ad31ac76144e9a4fdfbd32c2e7765b79f0ac4b
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-02 15:05:02 +0200

    Create Program.cs

---
+++ ProgramingBasics/For-Loop/SmartLili/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Dynamic;
+
+namespace SmartLili
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liliAge = int.Parse(Console.ReadLine());
+            double washingMachine = double.Parse(Console.ReadLine());
+            int toysPrice = int.Parse(Console.ReadLine());
+            int toysAmount = 0;
+            double money = 0.0;
+            double brothersMoney = 1;
+            double toysFullPrice = 0.0;
+            double moneyLeft = 0.0;
+            double moneyScale = 10.0;
+            for(int i = 1; i <= liliAge; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    toysAmount += 1;
+                }
+                if (i == 2)
+                {
+                    money = 10.00-brothersMoney;
+                    
+                }
+                else if (i % 2 == 0)
+                {
+                    moneyScale += 10;
+                    money = money + moneyScale - brothersMoney;
+                }
+                 
+            }
+            toysFullPrice = toysAmount * toysPrice;
+            money += toysFullPrice;
+            if (money >= washingMachine)
+            {
+                moneyLeft = money-washingMachine;
+                Console.WriteLine($"Yes! {moneyLeft:f2}");
+            }
+            else
+            {
+                moneyLeft = washingMachine - money;
+                Console.WriteLine($"No! {moneyLeft:f2}");
+            }
+        }
+    }
+}
