0a7f364abf81a6d46b72471206213c59237f9498
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-02 15:04:54 +0200

    Create Program.cs

---
+++ ProgramingBasics/For-Loop/NumbersSum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NumbersSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int randomNumbers = int.Parse(Console.ReadLine());
+            int result = 0;
+            for (int i = 0; i < randomNumbers; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                result += number;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
