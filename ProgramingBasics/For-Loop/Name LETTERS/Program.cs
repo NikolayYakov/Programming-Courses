6f1fd0d7179883b7a6bea0310e3b2bf897ecd537
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-02 15:04:42 +0200

    Create Program.cs

---
+++ ProgramingBasics/For-Loop/Name LETTERS/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Name_LETTERS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            for (int i= 0; i< name.Length; i += 1)
+            {
+                char letter = name[i];
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
