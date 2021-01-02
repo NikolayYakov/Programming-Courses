8b37e99613bd4c80621c74d203c6b1a771af4098
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-02 15:04:47 +0200

    Create Program.cs

---
+++ ProgramingBasics/For-Loop/Number 1 to 10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Number_1_to_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            for(int i = 1;i<=100;i ++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
