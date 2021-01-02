95abfc6465db1b8e5595f39e18a1112628136b1d
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-02 15:04:59 +0200

    Create Program.cs

---
+++ ProgramingBasics/For-Loop/NumbersTo1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NumbersTo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            for (int number = int.Parse(Console.ReadLine()); number >= 1; number -= 1) 
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
