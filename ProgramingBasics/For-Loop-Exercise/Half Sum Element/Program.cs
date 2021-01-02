5aae0416a6ac7c64c26fce07c88885a798bb53b3
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-02 15:03:46 +0200

    Create Program.cs

---
+++ ProgramingBasics/For-Loop-Exercise/Half Sum Element/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Half_Sum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int randomNumbers = int.Parse(Console.ReadLine());
+            int result = 0;
+            int maxNumber = int.MinValue;
+            for(int i = 1; i <= randomNumbers; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                result += number;
+                if (number > maxNumber)
+                {
+                    maxNumber = number;
+                }
+            }
+            result -= maxNumber;
+            if (result == maxNumber)
+            {
+                Console.WriteLine($"Yes");
+                Console.WriteLine($"Sum = {result}");
+
+            }
+            else
+            {
+                int difference = result - maxNumber;
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(difference)}");
+            }
+        }
+    }
+}
