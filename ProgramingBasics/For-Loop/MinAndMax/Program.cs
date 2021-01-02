6568b75b2734ee884b5188e1c7f152ed5bd5a0d5
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-02 15:04:37 +0200

    Create Program.cs

---
+++ ProgramingBasics/For-Loop/MinAndMax/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MinAndMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int randomNumbers = int.Parse(Console.ReadLine());
+            int number = 0;
+            int maxNumber = int.MinValue;
+            int minNumber = int.MaxValue;
+            for(int i = 0; i < randomNumbers; i += 1)
+            {
+                 number = int.Parse(Console.ReadLine());
+                if (maxNumber < number)
+                {
+                    maxNumber = number;
+                }
+                if (minNumber > number)
+                {
+                    minNumber = number;
+                }
+            }
+            Console.WriteLine($"Max number: {maxNumber}");
+            Console.WriteLine($"Min number: {minNumber}");
+        }
+    }
+}
