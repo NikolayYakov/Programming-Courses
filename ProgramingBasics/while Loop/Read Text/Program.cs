ac657489bc2347fc4584d2e21bff789a33378b80
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-03 14:50:41 +0200

    Create Program.cs

---
+++ ProgramingBasics/while Loop/Read Text/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Read_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string name = Console.ReadLine();
+                if (name == "Stop")
+                {
+                     
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(name);
+                }
+                
+            }
+        }
+    }
+}
