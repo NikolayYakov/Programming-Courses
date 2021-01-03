f8ba8d86d8f84ef3e2e6bbb96f52d0c9e477c6b3
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-03 14:49:14 +0200

    Create Program.cs

---
+++ ProgramingBasics/While Loop Exercise/Cake/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cakeHeight = int.Parse(Console.ReadLine());
+            int cakeWidth = int.Parse(Console.ReadLine());
+            int cakeSize = cakeHeight * cakeWidth;
+            string cakePieces = Console.ReadLine();
+            int cakeNeeded = 0;
+            int sum = 0;
+            while (cakePieces != "STOP")
+            {
+                int cakeRealPieces = int.Parse(cakePieces);
+                sum += cakeRealPieces;
+                if (sum >= cakeSize)
+                {
+                    cakeNeeded = sum - cakeSize;
+                    Console.WriteLine($"No more cake left! You need {cakeNeeded} pieces more.");
+                    break;
+                }
+                cakePieces = Console.ReadLine(); 
+            }
+            if (cakePieces == "STOP")
+            {
+                cakeNeeded = cakeSize - sum;
+                Console.WriteLine($"{cakeNeeded} pieces are left.");
+            }
+        }
+    }
+}
