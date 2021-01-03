73e72fdb5235e8280378270faa6548be5662adc4
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-03 14:50:46 +0200

    Create Program.cs

---
+++ ProgramingBasics/while Loop/Sequence 2k+1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sequence_2k_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int number0ne = 1;
+            while (number0ne<=number)
+            {
+                Console.WriteLine(number0ne);
+                number0ne = number0ne * 2 + 1;
+                 
+            }
+        }
+    }
+}
