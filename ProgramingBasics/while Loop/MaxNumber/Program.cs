e68658b32bead48b51cce135170e11d6da31cc47
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-03 14:50:15 +0200

    Create Program.cs

---
+++ ProgramingBasics/while Loop/MaxNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int maxValue = int.MinValue;
+            while (number != "Stop")
+            {
+                int realNumber = int.Parse(number);
+                if (maxValue < realNumber)
+                {
+                    maxValue = realNumber;
+                }
+                number = Console.ReadLine();
+                 
+            }
+            Console.WriteLine(maxValue);
+        }
+    }
+}
