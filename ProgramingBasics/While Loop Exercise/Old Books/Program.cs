8dc6c56ddd20474b398544097e3204a9a7086860
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-03 14:49:33 +0200

    Create Program.cs

---
+++ ProgramingBasics/While Loop Exercise/Old Books/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Old_Books
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string favoBook = Console.ReadLine();
+            int count = 0;
+            while (true)
+            {
+                string book = Console.ReadLine();
+                count += 1;
+                if (book == "No More Books")
+                {
+                    count -= 1;
+                    Console.WriteLine("The book you search is not here!");
+                    Console.WriteLine($"You checked {count} books.");
+                    break;
+                }
+                else if(book == favoBook)
+                {
+                    count -= 1;
+                    Console.WriteLine($"You checked {count} books and found it.");
+                    break;
+                }
+            }
+        }
+    }
+}
