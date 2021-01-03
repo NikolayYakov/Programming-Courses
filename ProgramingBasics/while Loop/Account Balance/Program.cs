95772891b9e80466761a2954f90444f14bc320cb
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-03 14:49:57 +0200

    Create Program.cs

---
+++ ProgramingBasics/while Loop/Account Balance/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data.SqlTypes;
+using System.Security.Cryptography;
+
+namespace Account_Balance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0.0;
+
+            string input = Console.ReadLine();
+            while (input!= "NoMoreMoney")
+            {
+                double money = double.Parse(input);
+                
+                
+                if (money >= 0)
+                {
+                    
+                    Console.WriteLine($"Increase: {money:f2}");
+                    sum += money;
+                    input = Console.ReadLine();
+                    
+                }
+               else if (money < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    Console.WriteLine($"Total: {sum:f2}");
+                    break;
+                }
+              
+                 
+                
+            }
+            if (input == "NoMoreMoney")
+            {
+                Console.WriteLine($"Total: {sum:f2}");
+            }
+        }
+    }
+}
