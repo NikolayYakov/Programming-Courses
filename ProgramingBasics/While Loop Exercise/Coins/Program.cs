f4d29a653312cd2dcd1b2e289af1c88bd2a3b3f9
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-03 14:49:18 +0200

    Create Program.cs

---
+++ ProgramingBasics/While Loop Exercise/Coins/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Coins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            
+            int coins = 0;
+            while (money > 0)
+            {
+                money = Math.Round(money, 2);
+                if (money >= 2)
+                {
+                    money -= 2;
+                    coins += 1;
+                }
+                else if (money >= 1)
+                {
+                    money -= 1;
+                    coins += 1;
+                }
+                else if (money >= 0.50)
+                {
+                    money -= 0.50;
+                    coins += 1;
+                }
+                else if (money >= 0.20)
+                {
+                    money -= 0.20;
+                    coins += 1;
+                }
+                else if (money >= 0.10)
+                {
+                    money -= 0.10;
+                    coins += 1;
+                }
+                else if (money >= 0.05)
+                {
+                    money -= 0.05;
+                    coins += 1;
+                }
+                else if (money >= 0.02)
+                {
+                    money -= 0.02;
+                    coins += 1;
+                }
+                else
+                {
+                    money -= 0.01;
+                    coins += 1;
+                }
+            }
+            Console.WriteLine(coins);
+        }
+    }
+}
