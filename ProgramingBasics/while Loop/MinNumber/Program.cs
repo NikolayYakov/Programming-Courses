26b30762e859dc48d013363491cbf967e43dde24
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-03 14:50:20 +0200

    Create Program.cs

---
+++ ProgramingBasics/while Loop/MinNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MinNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int minValue = int.MaxValue;
+            while (number != "Stop")
+            {
+                int realNumber = int.Parse(number);
+                if (minValue > realNumber)
+                {
+                    minValue = realNumber;
+                }
+                number = Console.ReadLine();
+
+            }
+            Console.WriteLine(minValue);
+        }
+    }
+}
