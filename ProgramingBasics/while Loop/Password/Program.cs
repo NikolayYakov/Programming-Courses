94aa14af83f93e3e059fdd17b5d2dea0ae27c3c2
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-03 14:50:38 +0200

    Create Program.cs

---
+++ ProgramingBasics/while Loop/Password/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = Console.ReadLine();
+            while (true)
+            {
+                string pass = Console.ReadLine();
+                if(pass == password)
+                {
+                    Console.WriteLine($"Welcome {username}!");
+                    break;
+                }
+
+            }
+        }
+    }
+}
