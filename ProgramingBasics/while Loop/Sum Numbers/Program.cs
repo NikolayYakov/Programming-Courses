18ac53f62e165da1493f4072f9c4b83ff197f9a3
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-03 14:50:51 +0200

    Create Program.cs

---
+++ ProgramingBasics/while Loop/Sum Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            while (true)
+            {
+                int secondNumber = int.Parse(Console.ReadLine());
+                sum += secondNumber;
+                if (sum >= number)
+                {
+                    Console.WriteLine(sum);
+                    break;
+                }
+            }
+        }
+    }
+}
