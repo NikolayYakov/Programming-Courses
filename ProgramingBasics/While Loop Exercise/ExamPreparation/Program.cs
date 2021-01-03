8d5a97afd86486fc304e52c1a6ace8e20d7434f6
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-03 14:49:24 +0200

    Create Program.cs

---
+++ ProgramingBasics/While Loop Exercise/ExamPreparation/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ExamPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int badGrades = int.Parse(Console.ReadLine());
+            int counter = 0;
+            double averageGrade = 0.0;
+            double projectCounter = 0.0;
+            string lastProject = "";
+            while (true)
+            {
+                string project = Console.ReadLine();
+                if(project== "Enough")
+                {
+                    averageGrade = averageGrade / projectCounter;
+                    Console.WriteLine($"Average score: {averageGrade:f2}");
+                    Console.WriteLine($"Number of problems: {projectCounter:f0}");
+                    Console.WriteLine($"Last problem: {lastProject}");
+                    break;
+                }
+                int grade = int.Parse(Console.ReadLine());
+                projectCounter += 1;
+                averageGrade += grade;
+                if (grade <= 4)
+                {
+                    counter += 1;
+                }
+                if (counter == badGrades)
+                {
+                    Console.WriteLine($"You need a break, {counter} poor grades.");
+                    break;
+                }
+                lastProject = project;
+            }
+        }
+    }
+}
