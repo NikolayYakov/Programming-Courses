00b4de0c63f0c5698a4538602c04354cdbb9f51c
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-03 14:50:03 +0200

    Create Program.cs

---
+++ ProgramingBasics/while Loop/graduation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentName = Console.ReadLine();
+            int counter = 0;
+            int yearcounter = 0;
+            double averageGrade = 0.0;
+            
+            
+            while (counter<2)
+            {
+                double yearGrade = double.Parse(Console.ReadLine());
+                yearcounter += 1;
+                averageGrade += yearGrade;
+                if (yearGrade < 4)
+                {
+                    counter += 1;
+                }
+                if (yearcounter >= 12) 
+                {
+                    averageGrade /= 12.0;
+                    Console.WriteLine($"{studentName} graduated. Average grade: {averageGrade:f2}");
+                    break;
+                }
+
+            }
+            if (counter == 2)
+            {
+                yearcounter -= 1;
+                Console.WriteLine($"{studentName} has been excluded at {yearcounter} grade");
+            }
+           
+        }
+    }
+}
