cbd70f34a690e968814de8b2e39b9957d7df8705
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-04 14:24:23 +0200

    Create Program.cs

---
+++ ProgramingBasics/Naste loops exercise/Number Pyramid/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Number_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int counter = 0;
+            bool flag = false;
+            int curentNumber = 0;
+            for(int r = 1; r <= number; r++)
+            {
+                for(int col = 1; col <= r; col++)
+                {
+                    counter++;
+                    if (counter > number)
+                    {
+                        flag = true;
+                        break;
+                    }
+                    curentNumber++;
+                    Console.Write($"{curentNumber} ");
+                }
+                if (flag == true)
+                {
+                    break;
+                }
+                Console.WriteLine("");
+            }
+        } 
+    }
+}
