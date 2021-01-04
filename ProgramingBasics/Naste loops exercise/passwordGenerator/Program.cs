db332a9f18be7947764cc51650a738e6b2a0824c
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-04 14:24:28 +0200

    Create Program.cs

---
+++ ProgramingBasics/Naste loops exercise/passwordGenerator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace passwordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());
+            
+            for(int number1 = 1; number1 <= n; number1++)
+            {
+                for(int number2 = 1; number2 <= n; number2++) 
+                {
+
+                    for(char alpha = 'a'; alpha <'a'+l ; alpha++)
+                    {
+                       
+                        for(char betta = 'a'; betta < 'a'+l; betta++)
+                        {
+                            
+                            for (int finalNumber = 1; finalNumber <= n; finalNumber++)
+                            {
+                                    if(finalNumber > number1 && finalNumber > number2)
+                                    {
+                                    Console.Write($"{number1}{number2}{alpha}{betta}{finalNumber} ");
+                                    }
+                            }
+
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+}
