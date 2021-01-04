cb64c5b14523c387d2fae5665f5a1a6b4985756f
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-04 14:24:15 +0200

    Create Program.cs

---
+++ ProgramingBasics/Naste loops exercise/Equal Sums Even Odd Position/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Equal_Sums_Even_Odd_Position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            
+            for(int n = firstNumber; n <= secondNumber; n++)
+            {
+                int evenSum = 0;
+                int oddSum = 0;
+                string curentNumber = n.ToString();
+
+                for(int i = 0; i <curentNumber.Length; i++)
+                {
+                    int curentDigit = int.Parse(curentNumber[i].ToString());
+                    if (i % 2 == 0)
+                    {
+                        evenSum += curentDigit;
+                    }
+                    else
+                    {
+                        oddSum += curentDigit;
+                    }
+
+ 
+                }
+                if (evenSum == oddSum)
+                {
+                    Console.Write($"{n} ");
+                }
+            }
+           
+        }
+    }
+}
