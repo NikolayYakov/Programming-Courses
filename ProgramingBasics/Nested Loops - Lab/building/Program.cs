22f1a57c09817d081fe67b0676e9f1ad3dd6ba6f
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-04 14:24:51 +0200

    Create Program.cs

---
+++ ProgramingBasics/Nested Loops - Lab/building/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int floars = int.Parse(Console.ReadLine());
+            int rooms = int.Parse(Console.ReadLine());
+            for(int f = floars; f != 0; f--)
+            {
+                for(int r=0; r<rooms;r++) 
+                {
+                    if (f == floars)
+                    {
+                        Console.Write($"L{f}{r} ");
+                         
+                    }
+                    
+                    else if (f % 2 == 0)
+                    {
+                        Console.Write($"O{f}{r} ");
+                    }
+                    else
+                    {
+                        Console.Write($"A{f}{r} ");
+                    }
+                    if (r == rooms - 1)
+                    {
+                        Console.WriteLine("");
+                    }
+                }
+            }
+ 
+        }
+    }
+}
