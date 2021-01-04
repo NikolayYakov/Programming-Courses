5a984eb0ab658f197a52c01a865557d233886a2e
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-04 14:25:08 +0200

    Create Program.cs

---
+++ ProgramingBasics/Nested Loops - Lab/Combinations/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int counter = 0;
+            int number = int.Parse(Console.ReadLine());
+            for(int i = 0; i <= number;i++)
+            {
+                for(int k = 0; k <= number; k++)
+                {
+                    for(int j = 0; j <= number; j++)
+                    {
+                        sum = i + k + j;
+                        if (sum == number)
+                        {
+                            counter++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+         
+        }
+    }
+}
