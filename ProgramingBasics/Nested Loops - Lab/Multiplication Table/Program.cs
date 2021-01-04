8dab17fe786ef94d9fdb25659ecd4de20957f9e4
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-04 14:25:14 +0200

    Create Program.cs

---
+++ ProgramingBasics/Nested Loops - Lab/Multiplication Table/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            int result = 0;
+            for(int i = 1; i <= 10; i++) 
+            {
+                for(int j = 1; j <= 10; j++)
+                {
+                    result = i * j;
+                    Console.WriteLine($"{i} * {j} = {result}");
+                }
+            }
+        }
+    }
+}
