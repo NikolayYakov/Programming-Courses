9c469ddaad70d932c2c4dd05ba84b504adecebfd
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-04 14:25:22 +0200

    Create Program.cs

---
+++ ProgramingBasics/Nested Loops - Lab/Traveling/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Traveling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            while (true)
+            {
+                string destination = Console.ReadLine();
+                
+                if (destination == "End")
+                {
+                    break;
+                }
+                double budget = double.Parse(Console.ReadLine());
+                double money = 0.0;
+                while (money < budget )
+                {
+                    double income = double.Parse(Console.ReadLine());
+                    money += income;
+                }
+                Console.WriteLine($"Going to {destination}!");
+                 
+                 
+             
+                 
+                
+
+            }
+        }
+    }
+}
