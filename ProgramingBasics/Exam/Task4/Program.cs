8e2bcc0be037e2ea0d7078752fbe5d8a95ce7db8
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-06 23:39:25 +0200

    Create Program.cs

---
+++ ProgramingBasics/Exam/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int months = int.Parse(Console.ReadLine());
+            double simpleInterestMoney = money;
+            double complexInterestMoney = money;
+            double simpleInterest = money;
+            double complexInterest = money;
+            for(int n = 1; n <= months; n++)
+            {
+                simpleInterest *= 0.03 ;
+                simpleInterestMoney += simpleInterest;
+                complexInterest *= 0.027;
+                complexInterestMoney += complexInterest;
+                simpleInterest
+            }
+        }
+    }
+}
