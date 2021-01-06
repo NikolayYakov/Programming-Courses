908560497d3108dc4f63e4aa62e47b620cde3af2
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-06 23:39:37 +0200

    Create Program.cs

---
+++ ProgramingBasics/Exam/Task6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            int counterDays = 1;
+            int counterMeters = 5364;
+            while (true)
+            {
+                string day = Console.ReadLine();
+                if (day == "END")
+                {
+                    Console.WriteLine("Failed!");
+                    Console.WriteLine($"{counterMeters}");
+                    break;
+                }
+                if (day == "Yes")
+                {
+                    counterDays++;
+                }
+                
+                int meters =int.Parse(Console.ReadLine());
+                counterMeters += meters;
+                if(counterMeters >= 8848)
+                {
+                    Console.WriteLine($"Goal reached for {counterDays} days!");
+                    break;
+                }
+                if (counterDays > 5)
+                {
+                    counterMeters -= meters;
+                    Console.WriteLine("Failed!");
+                    Console.WriteLine($"{counterMeters}");
+                    break;
+                }
+            }
+        }
+    }
+}
