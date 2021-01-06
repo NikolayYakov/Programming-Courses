6052298e826206dd43f9700601c8f52811064b2f
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-06 23:39:12 +0200

    Create Program.cs

---
+++ ProgramingBasics/Exam/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cpuPrice = double.Parse(Console.ReadLine());
+            double cardPrice = double.Parse(Console.ReadLine());
+            double ramPrice = double.Parse(Console.ReadLine());
+            double ramAmount = double.Parse(Console.ReadLine());
+            double discount = double.Parse(Console.ReadLine());
+            cpuPrice *= 1.57;
+            cardPrice *= 1.57;
+            ramPrice *= 1.57;
+            ramPrice *= ramAmount;
+            double cardDiscount = cardPrice * discount;
+            cardPrice -= cardDiscount;
+            double cpuDiscount = cpuPrice * discount;
+            cpuPrice -= cpuDiscount;
+            double fullPrice = cpuPrice + cardPrice + ramPrice;
+            Console.WriteLine($"Money needed - {fullPrice:F2} leva.");
+        }
+    }
+}
