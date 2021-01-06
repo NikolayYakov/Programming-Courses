54029eae2cae92093746eac665c0215b2a492d66
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-06 23:40:26 +0200

    Create Program.cs

---
+++ ProgramingBasics/PracticeExam/safari/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace safari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            double gas = double.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+            double moneyNeeded = 0.0;
+            gas *= 2.10;
+            moneyNeeded += gas;
+            moneyNeeded += 100;
+            switch (day)
+            {
+                case "Saturday":
+                    moneyNeeded *= 0.90;
+                    break;
+                case "Sunday":
+                    moneyNeeded *= 0.80;
+                    break;
+            }
+           double moneyLeft = Math.Abs(budget - moneyNeeded);
+            if (budget >= moneyNeeded)
+            {
+                Console.WriteLine($"Safari time! Money left: {moneyLeft:f2} lv.");
+              
+
+
+
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! Money needed: {moneyLeft:f2} lv. ");
+                
+            }
+        }
+    }
+}
