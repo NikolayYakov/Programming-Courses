682d06dc8c4be59fe12479adb2501292423a3f94
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-06 23:39:16 +0200

    Create Program.cs

---
+++ ProgramingBasics/Exam/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            int ilindenHealth = int.Parse(Console.ReadLine());
+            power *= people;
+            int difference = Math.Abs(power - ilindenHealth);
+            if (power < ilindenHealth)
+            {
+                Console.WriteLine($"You are not prepared! You need {difference} more points.");
+                
+
+            }
+            else
+            {
+                Console.WriteLine($"Illidan has been slain! You defeated him with {difference} points.");
+            }
+        }
+    }
+}
