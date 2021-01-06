6c87c9d448615fdc929a8c280d0b0d447414758e
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-06 23:39:58 +0200

    Create Program.cs

---
+++ ProgramingBasics/PracticeExam/DividedBy/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DividedBy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            double dividedBy2 = 0.0;
+            double dividedBy3 = 0.0;
+            double dividedBy4 = 0.0;
+            double counter = 0.0;
+            for (int n = 1; n <= number; n++)
+            {
+                int realNumber = int.Parse(Console.ReadLine());
+                counter += 1.0;
+                if (realNumber % 2 == 0)
+                {
+                    dividedBy2 += 1.0;
+                }
+                if (realNumber % 3 == 0)
+                {
+                    dividedBy3 += 1.0;
+                }
+                if (realNumber % 4 == 0)
+                {
+                    dividedBy4 += 1.0;
+                }
+            }
+            double dividedBy2Percent = dividedBy2 / counter * 100.0;
+            double dividedBy3Percent = dividedBy3 / counter * 100.0;
+            double dividedBy4Percent = dividedBy4 / counter * 100.0;
+            Console.WriteLine($"{dividedBy2Percent:f2}%");
+            Console.WriteLine($"{dividedBy3Percent:f2}%");
+            Console.WriteLine($"{dividedBy4Percent:f2}%");
+        }
+    }
+}
