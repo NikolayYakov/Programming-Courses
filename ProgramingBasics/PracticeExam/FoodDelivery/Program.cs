db421e362bc2e6ccbaab12113e93338810224065
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-06 23:40:03 +0200

    Create Program.cs

---
+++ ProgramingBasics/PracticeExam/FoodDelivery/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FoodDelivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double chicken = double.Parse(Console.ReadLine());
+            double fish = double.Parse(Console.ReadLine());
+            double vega = double.Parse(Console.ReadLine());
+            double fullPrice = chicken * 10.35 + fish * 12.40 + vega * 8.15;
+            fullPrice *= 1.20;
+            fullPrice += 2.50;
+            Console.WriteLine($"Total: {fullPrice:f2}");
+        }
+    }
+}
