c2387ad945734f5c259b0abc327cbe6856e23c4a
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-07 17:33:23 +0200

    Create Program.cs

---
+++ Programming Fundamentals/VariablesPractice/ASCII Sum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ASCII_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int rows = int.Parse(Console.ReadLine());
+           int sum = 0;
+           for(int i = 1; i <= rows; i++)
+            {
+                char letter = char.Parse(Console.ReadLine());
+                sum += letter;
+            }
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
