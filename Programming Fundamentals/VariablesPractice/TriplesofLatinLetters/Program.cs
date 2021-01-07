47cde05543db3ab8aa9161fc54f04e8fc822f1e8
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-07 17:34:10 +0200

    Create Program.cs

---
+++ Programming Fundamentals/VariablesPractice/TriplesofLatinLetters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TriplesofLatinLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLetters = int.Parse(Console.ReadLine());
+            for(int i = 0; i < numberOfLetters; i++)
+            {
+                for (int k = 0; k < numberOfLetters; k++)
+                {
+                    for (int p= 0; p < numberOfLetters; p++)
+                    {
+                        char firstLetter = (char)('a' + i);
+                        char secondLetter = (char)('a' + k);
+                        char finalLetter = (char)('a' + p);
+                        Console.WriteLine($"{firstLetter}{secondLetter}{finalLetter}");
+                    }
+                }
+            }
+        }
+    }
+}
