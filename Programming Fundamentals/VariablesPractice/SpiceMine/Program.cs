a6bf98d56424b19ef8e274d42d3849ef57482e1b
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-07 17:33:56 +0200

    Create Program.cs

---
+++ Programming Fundamentals/VariablesPractice/SpiceMine/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SpiceMine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yield = int.Parse(Console.ReadLine());
+            int allMined = 0;
+            int counter = 0;
+            while (yield >= 100)
+            {
+                allMined += yield;
+                yield -= 10;
+                allMined -= 26;
+                counter += 1;
+            }
+            if (allMined >= 26)
+            {
+                allMined -= 26;
+
+            }
+            else
+            {
+                allMined = 0;
+            }
+            Console.WriteLine(counter);
+            Console.WriteLine(allMined);
+            
+
+        }
+    }
+}
