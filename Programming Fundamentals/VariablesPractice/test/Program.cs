975292af2a2878fd55e07c0338b8671313983b59
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-07 17:34:06 +0200

    Create Program.cs

---
+++ Programming Fundamentals/VariablesPractice/test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = (10 / 2);
+            sum = Math.Pow(sum,3l);
+              
+            Console.WriteLine(sum);
+        }
+    }
+}
