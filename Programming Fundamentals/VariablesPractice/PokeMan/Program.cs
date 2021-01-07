ee79bcc9e8cba4f3457ccc3455dfe31dc70466c9
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-07 17:33:42 +0200

    Create Program.cs

---
+++ Programming Fundamentals/VariablesPractice/PokeMan/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PokeMan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long pokePower = long.Parse(Console.ReadLine());
+            long originalPokePower = pokePower; 
+            long distance = long.Parse(Console.ReadLine());
+            long exhaustionFactor = long.Parse(Console.ReadLine());
+            long pokeTarget = 0;  
+            while (distance<=pokePower)
+            {
+                if ( originalPokePower*0.50==pokePower && exhaustionFactor!=0)
+                {
+                    pokePower /= exhaustionFactor;
+                }
+                if (distance > pokePower)
+                {
+                    break;
+                }
+                
+                    pokePower -= distance;
+                    pokeTarget++;
+             
+               
+                
+            }
+            Console.WriteLine(pokePower);
+            Console.WriteLine(pokeTarget);
+        }
+    }
+}
