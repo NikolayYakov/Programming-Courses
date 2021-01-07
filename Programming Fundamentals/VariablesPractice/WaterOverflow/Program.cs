cf695f7355195fa58a06b84c166272601b31fb47
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-07 17:34:16 +0200

    Create Program.cs

---
+++ Programming Fundamentals/VariablesPractice/WaterOverflow/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WaterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double waterTankCapacity = 255;
+            double waterSum = 0;
+            int fills = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= fills; i++)
+            {
+                double addWater = double.Parse(Console.ReadLine());
+                waterSum += addWater;
+                if (waterSum > waterTankCapacity)
+                {
+                    waterSum -= addWater;
+                    Console.WriteLine("Insufficient capacity!");
+                }
+            }
+            Console.WriteLine(waterSum);
+        }
+    }
+}
