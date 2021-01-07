4036ebd1a5e2ff3160dac86df199db5f73895fb3
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-07 17:33:38 +0200

    Create Program.cs

---
+++ Programming Fundamentals/VariablesPractice/ElevatoCourses/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ElevatoCourses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int elevatorCapacity = int.Parse(Console.ReadLine());
+            int courses = 0;
+            while (true)
+            {
+                if (people > 0)
+                {
+                    courses += 1;
+                    people -= elevatorCapacity;
+                }
+                else
+                {
+                    Console.WriteLine(courses);
+                    break;
+                }
+            }
+        }
+    }
+}
