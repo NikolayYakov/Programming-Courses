58314a10c7c8bdac7c093af82b0cf34f8906aaec
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-07 17:33:47 +0200

    Create Program.cs

---
+++ Programming Fundamentals/VariablesPractice/PrintPartOfTheASCIITable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PrintPartOfTheASCIITable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int begining = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            for (int i = begining; i <= end; i++)
+            {
+                char symbol = (char)i;
+                Console.Write($"{symbol} ");
+            }
+        }
+    }
+}
