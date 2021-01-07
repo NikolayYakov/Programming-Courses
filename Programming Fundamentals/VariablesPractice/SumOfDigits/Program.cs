2008650ee952bb832c81399862843ff6241c36c8
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-07 17:34:01 +0200

    Create Program.cs

---
+++ Programming Fundamentals/VariablesPractice/SumOfDigits/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumOfDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int digits = number;
+            int sum = 0;
+            while (number > 0)
+            {
+                digits = number % 10;
+                sum += digits;
+                number /= 10;
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
