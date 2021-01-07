685762a22f8ef13f5604b5a67d2f9c3553a1e860
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-07 17:33:50 +0200

    Create Program.cs

---
+++ Programming Fundamentals/VariablesPractice/Snowballs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+
+namespace Snowballs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfShowBalls = int.Parse(Console.ReadLine());
+            BigInteger sum = 0;
+
+            BigInteger biggestSum = 0;
+            int biggestSnow = 0;
+            int biggestTime = 0;
+            int biggestQuality = 0;
+            for(int i = 1; i <= numberOfShowBalls; i++)
+            {
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+                int divided = (snowballSnow / snowballTime);
+
+                 sum = BigInteger.Pow(divided, snowballQuality);
+               
+                if (sum > biggestSum)
+                {
+                    biggestSum = sum; 
+                    biggestTime = snowballTime;
+                    biggestSnow = snowballSnow;
+                    biggestQuality = snowballQuality;
+                }
+
+            }
+            Console.WriteLine($"{biggestSnow} : {biggestTime} = {biggestSum} ({biggestQuality})");
+        }
+    }
+}
