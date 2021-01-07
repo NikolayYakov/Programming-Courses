4247c78338b6b137f741806c7e24bf4c6cee56c4
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-07 17:33:31 +0200

    Create Program.cs

---
+++ Programming Fundamentals/VariablesPractice/BeerKegs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int beerKentAmount = int.Parse(Console.ReadLine());
+            var biggestVolume = 0.0;
+            string biggestModel = string.Empty;
+            for (int i = 1; i<= beerKentAmount; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                var volume = Math.PI * radius * radius * height;
+                if (volume > biggestVolume)
+                {
+                    biggestVolume = volume;
+                    biggestModel = model;
+                }
+
+            }
+            Console.WriteLine(biggestModel);
+        }
+    }
+}
