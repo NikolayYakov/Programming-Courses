b7c0f7c494a98edcb98039f61c8db8336af2af92
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-07 17:33:15 +0200

    Create Program.cs

---
+++ Programming Fundamentals/VariablesPractice/4Number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _4Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int lastNumber = int.Parse(Console.ReadLine());
+            int sum = 0;
+            sum += firstNumber;
+            sum += secondNumber;
+            sum /= thirdNumber;
+            sum *= lastNumber;
+            Console.WriteLine(sum);
+        }
+    }
+}
