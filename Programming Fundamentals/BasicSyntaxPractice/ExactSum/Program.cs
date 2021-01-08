2c1188d9aba8e6219c2109fb51fea3711129cd47
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-08 17:14:17 +0200

    Create Program.cs

---
+++ Programming Fundamentals/BasicSyntaxPractice/ExactSum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExactSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal sum = 0M;
+            for(int i = 1; i <= n; i++)
+            {
+                
+                sum += decimal.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
