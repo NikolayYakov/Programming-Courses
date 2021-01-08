ad797f696fc5948ce3c472f354dcfba2ee59f57a
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-08 17:14:51 +0200

    Create Program.cs

---
+++ Programming Fundamentals/BasicSyntaxPractice/PyramidOfNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PyramidOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            for(int rows = 1; rows <= number; rows++)
+            {
+                for(int colums = 1; colums <= rows; colums++)
+                {
+                    Console.Write($"{rows} ");
+                    
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
