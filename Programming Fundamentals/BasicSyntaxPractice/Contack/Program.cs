69bb6904122d99580fa69d836504506c802aeef2
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-08 17:14:06 +0200

    Create Program.cs

---
+++ Programming Fundamentals/BasicSyntaxPractice/Contack/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Contack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            string contact = Console.ReadLine();
+            Console.WriteLine($"{firstName}{contact}{lastName}");
+        }
+    }
+}
