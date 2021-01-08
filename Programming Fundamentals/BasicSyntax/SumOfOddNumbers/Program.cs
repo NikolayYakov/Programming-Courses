2e3541451bcd0b8a010f422f1ed0f906d7471005
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-08 17:13:31 +0200

    Create Program.cs

---
+++ Programming Fundamentals/BasicSyntax/SumOfOddNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SumOfOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amount = int.Parse(Console.ReadLine());
+            var sum = 0;
+            var counter = 0;
+            for(int i = 1 ; i>0; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                    sum += i;
+                    counter += 1;
+                }
+                if (counter == amount)
+                {
+                    break;
+                }
+                
+                
+               
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
