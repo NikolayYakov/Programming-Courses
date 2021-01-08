f4b7794270239de5b44a8e476444815db78bb45e
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-08 17:14:29 +0200

    Create Program.cs

---
+++ Programming Fundamentals/BasicSyntaxPractice/Logic/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = string.Empty;
+            var counter = 0;
+            for (int i =username.Length-1; i >= 0; i--)
+            {
+                password += username[i];
+            }
+            while (true)
+            {
+                var fillPassword = Console.ReadLine();
+                if (fillPassword == password)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    break;
+                }
+                else if (counter == 3)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                    counter++;
+                }
+            }
+        }
+    }
+}
