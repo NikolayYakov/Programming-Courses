e08156f3972eee31ebc765336305a4891ab03ae6
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-08 17:13:08 +0200

    Create Program.cs

---
+++ Programming Fundamentals/BasicSyntax/MultiplationTable 2.0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MultiplationTable_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var integer = int.Parse(Console.ReadLine());
+            var times = int.Parse(Console.ReadLine());
+            var sum = 0;
+            for(int i = times; i <= 10; i++)
+            {
+                sum = integer * i;
+                Console.WriteLine($"{integer} X {i} = {sum}");
+
+            }
+            if (times > 10)
+            {
+                sum = integer * times;
+                Console.WriteLine($"{integer} X {times} = {sum}");
+            }
+        }
+    }
+}
