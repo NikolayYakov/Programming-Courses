e3c732747f4029bb20a708f16254b6325fdaeb70
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-08 17:15:17 +0200

    Create Program.cs

---
+++ Programming Fundamentals/BasicSyntaxPractice/TownInfo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TownInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cityName = Console.ReadLine();
+            int popilation = int.Parse(Console.ReadLine());
+            int size = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Town {cityName} has population of {popilation} and area {size} square km.");
+        }
+    }
+}
