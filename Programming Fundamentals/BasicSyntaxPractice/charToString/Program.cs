1d0ef596b8cb07e4a51b692c085d94fa3e6a1582
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-08 17:13:57 +0200

    Create Program.cs

---
+++ Programming Fundamentals/BasicSyntaxPractice/charToString/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace charToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = char.Parse(Console.ReadLine());
+            char b = char.Parse(Console.ReadLine());
+            char c = char.Parse(Console.ReadLine());
+            Console.WriteLine($"{a}{b}{c}");
+
+        }
+    }
+}
