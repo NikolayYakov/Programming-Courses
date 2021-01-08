c95f140ce602439f326b6dae20e7ea56dba44d8d
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-08 17:12:40 +0200

    Create Program.cs

---
+++ Programming Fundamentals/BasicSyntax/After30Minutes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace After30Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            minutes += 30;
+            if (minutes >= 60)
+            {
+                hour += 1;
+                minutes -= 60;
+            }
+            if (hour > 23)
+            {
+                hour = 0;
+            }
+            Console.WriteLine($"{hour}:{minutes:D2}");
+
+        }
+    }
+}
