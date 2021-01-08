cb213c53b14f6bbfc4dae7c6dbfc3684f57cf077
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-08 17:15:06 +0200

    Create Program.cs

---
+++ Programming Fundamentals/BasicSyntaxPractice/SpecialNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SpecialNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            for(int i =1;i<=n;i++)
+            {
+                int realNumber = i;
+                int digit = i ;
+                int sum = 0;
+                while (i > 0)
+                {
+                    digit = i % 10;
+                    sum += digit;
+                    i = i / 10;
+                  
+                    
+                    
+
+                }
+                if (sum==5||sum==7||sum==11)
+                {
+                    Console.WriteLine($"{realNumber} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{realNumber} -> False");
+                }
+                i = realNumber;
+           
+                
+            }
+ 
+        }
+    }
+}
