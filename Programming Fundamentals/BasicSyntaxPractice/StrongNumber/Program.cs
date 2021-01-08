0ff312676d6b2155707153f5a1ccf6fafead9f75
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-08 17:15:12 +0200

    Create Program.cs

---
+++ Programming Fundamentals/BasicSyntaxPractice/StrongNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = int.Parse(Console.ReadLine());
+            var number = input;
+            var currentNumber = 0;
+            var factorialSum = 0;
+            var factorial = 1;
+            while (number != 0)
+            {
+                currentNumber = number % 10;
+                number /= 10;
+                for(int i = 1; i <=currentNumber; i++)
+                {
+                   
+                    factorial *= i;
+                    
+                }
+                factorialSum += factorial;
+                factorial = 1;
+            }
+           
+            
+            if (input == factorialSum)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
