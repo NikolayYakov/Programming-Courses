54b515971d8f8243e4b3f51a8cd051210f7e88cc
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-08 17:12:55 +0200

    Create Program.cs

---
+++ Programming Fundamentals/BasicSyntax/EvenNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                var number = int.Parse(Console.ReadLine());
+                if (number % 2 != 0)
+                {
+                    Console.WriteLine("Please write an even number.");
+                }
+                else
+                {
+                    number = Math.Abs(number);
+                    Console.WriteLine($"The number is: {number}");
+                    break;
+                }
+            }
+        }
+    }
+}
