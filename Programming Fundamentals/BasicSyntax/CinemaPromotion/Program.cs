8c775563a3935e481357e0c6b2e8607d1737024a
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-08 17:12:44 +0200

    Create Program.cs

---
+++ Programming Fundamentals/BasicSyntax/CinemaPromotion/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace CinemaPromotion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string time = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            var price = 0;
+            switch (time)
+            {
+                case "Weekday":
+                    if (age >= 0 && age <= 18)
+                    {
+                        price = 12;
+                        Console.WriteLine($"{price}$");
+                    }
+                    else if (age < 0 || age > 122)
+                    {
+                        Console.WriteLine("Error!");
+                    }
+                    else if (age <= 64)
+                    {
+                        price = 18;
+                        Console.WriteLine($"{price}$");
+
+                    }
+                    else if (age <= 122)
+                    {
+                        price = 12;
+                        Console.WriteLine($"{price}$");
+
+                    }
+                    
+                    break;
+                case "Weekend":
+                    if (age >= 0 && age <= 18)
+                    {
+                        price = 15;
+                        Console.WriteLine($"{price}$");
+                    }
+                    else if (age < 0 || age > 122)
+                    {
+                        Console.WriteLine("Error!");
+                    }
+                    else if (age <= 64)
+                    {
+                        price = 20;
+                        Console.WriteLine($"{price}$");
+
+                    }
+                    else if (age <= 122)
+                    {
+                        price = 15;
+                        Console.WriteLine($"{price}$");
+
+                    }
+                    
+                    break;
+                case "Holiday":
+                    if (age >= 0 && age <= 18)
+                    {
+                        price = 5;
+                        Console.WriteLine($"{price}$");
+                    }
+                    else if (age < 0 || age > 122)
+                    {
+                        Console.WriteLine("Error!");
+                    }
+                    else if (age <= 64)
+                    {
+                        price = 12;
+                        Console.WriteLine($"{price}$");
+
+                    }
+                    else if (age <= 122)
+                    {
+                        price = 10;
+                        Console.WriteLine($"{price}$");
+
+                    }
+                    
+                    break;
+            }
+        }
+    }
+}
