c0c32dd40e103d622870dca74c3f96c4d2802a7a
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-08 17:14:44 +0200

    Create Program.cs

---
+++ Programming Fundamentals/BasicSyntaxPractice/PrintAndSum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrintAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = int.Parse(Console.ReadLine());
+            var end = int.Parse(Console.ReadLine());
+            var sum = 0;
+            for(int i = start; i <= end; i++)
+            {
+                Console.Write($"{i} ");
+                sum += i;
+            }
+            Console.WriteLine("");
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
