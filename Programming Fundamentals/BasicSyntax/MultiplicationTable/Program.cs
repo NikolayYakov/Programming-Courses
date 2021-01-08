6676468c93c4e8e2c997ca528a47e22aabff957f
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-08 17:13:12 +0200

    Create Program.cs

---
+++ Programming Fundamentals/BasicSyntax/MultiplicationTable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            
+            for(int i = 1; i <= 10; i++)
+            {
+                var sum = 0;
+                sum = number * i;
+                Console.WriteLine($"{number} X {i} = {sum}");
+                
+            }
+        }
+    }
+}
