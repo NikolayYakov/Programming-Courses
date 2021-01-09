a76f4f8329ad464fe0242f1815755bb697538150
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-09 11:53:25 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Arrays/SpecialNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+namespace SpecialNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+            int sum = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int k = i + 1; k < arr.Length; k++)
+                {
+
+                    bool check = arr[i] + arr[k] == sum;
+                    if (check == true)
+                    {
+                        Console.WriteLine($"{arr[i]} {arr[k]}");
+                    }
+                }
+            }
+        }
+    }
+}
