857134e889b7f1530d87c605f76f851838d2c169
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-09 11:52:34 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Arrays/CommonElements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+namespace CommonElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArr = Console.ReadLine()
+                                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                .Select(Convert.ToString)
+                                .ToArray();
+            string[] secondArr = Console.ReadLine()
+                                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                .Select(Convert.ToString)
+                                .ToArray();
+            string finalSentence = string.Empty;
+            for(int i = 0; i < secondArr.Length; i++)
+            {
+                for(int k = 0; k < firstArr.Length; k++)
+                {
+                    if (secondArr[i] == firstArr[k])
+                    {
+                        finalSentence += (firstArr[k]+ " ");
+                        
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(finalSentence);
+        }
+    }
+}
