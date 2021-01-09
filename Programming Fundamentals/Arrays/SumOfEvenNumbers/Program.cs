1bd7f72782dc7461a1c2f36e3b43e6beeeb0bdbc
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-09 11:53:30 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Arrays/SumOfEvenNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+namespace SumOfEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersArr = Console.ReadLine()
+                               .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                               .Select(int.Parse)
+                               .ToArray();
+            int result = 0;
+            for(int i = 0; i < numbersArr.Length; i++)
+            {
+                if (numbersArr[i] % 2 == 0)
+                {
+                    result += numbersArr[i];
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
