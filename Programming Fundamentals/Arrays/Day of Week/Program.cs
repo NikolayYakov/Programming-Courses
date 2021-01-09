8df5c5ba569015dbc7f849dfc9a55285cf75c6dc
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-09 11:52:45 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Arrays/Day of Week/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayNumber = int.Parse(Console.ReadLine());
+            string[] daysOfTheWeek = new string[]
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday",
+            };
+            if (dayNumber <= 7 && dayNumber >= 1)
+            {
+                Console.WriteLine(daysOfTheWeek[dayNumber - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+            
+        }
+    }
+}
