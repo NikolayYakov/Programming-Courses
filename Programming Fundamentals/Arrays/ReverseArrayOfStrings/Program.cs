ea49755c3098ef37b046b167964e561105dcf017
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-09 11:53:15 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Arrays/ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+namespace ReverseArrayOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] reveredString = Console.ReadLine()
+                                     .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                     .Select(Convert.ToString)
+                                     .ToArray();
+            Console.WriteLine(string.Join(' ', reveredString.Reverse()));
+                                     
+        }
+    }
+}
