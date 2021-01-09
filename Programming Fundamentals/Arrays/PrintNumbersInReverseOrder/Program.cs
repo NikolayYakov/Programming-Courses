0fc36b0b55c4bede0ae57f821d116a39c8e6bb22
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-09 11:53:12 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Arrays/PrintNumbersInReverseOrder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+namespace PrintNumbersInReverseOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amountOfNumbers = int.Parse(Console.ReadLine());
+            int[] arrNumbers = new int[amountOfNumbers];
+            for (int i = 0; i < amountOfNumbers; i++)
+            {
+                arrNumbers[i] = int.Parse(Console.ReadLine());
+            }
+             
+            Console.WriteLine(string.Join(' ',arrNumbers.Reverse()));
+        }
+    }
+}
