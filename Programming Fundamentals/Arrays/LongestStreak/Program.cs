bdc90988c91a56968d4b4aa177b41f7bfa831c63
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-09 11:53:01 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Arrays/LongestStreak/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LongestStreak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine()
+                           .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int bestIndex = 0;
+            int bestCount = 0;
+            for (int i=0; i < arr.Length; i++)
+            {
+                string currentElement = arr[i];
+                int currentCounter = 1;
+                
+                for (int k = i + 1; k < arr.Length; k++)
+                {
+                    
+                    if (currentElement == arr[k])
+                    {
+                        currentCounter++;
+                        
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    i = k; 
+                }
+                if (currentCounter > bestCount)
+                {
+                    bestIndex = i;
+                    bestCount=currentCounter;
+                }
+                 
+            }
+            for (int i = 0; i < bestCount; i++)
+            {
+                Console.Write(arr[bestIndex] + " ");
+            }
+        }
+    }
+}
