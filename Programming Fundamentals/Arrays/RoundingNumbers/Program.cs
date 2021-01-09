f6a266cc023a6825baae7e9bdf811d27abf3dfba
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-09 11:53:20 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Arrays/RoundingNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+namespace RoundingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numberArr = Console.ReadLine()
+                              .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                              .Select(double.Parse)
+                              .ToArray();
+            for(int i = 0; i < numberArr.Length; i++)
+            {
+                double number = numberArr[i];
+                double roundedeNumber = Math.Round(number, MidpointRounding.AwayFromZero);
+                Console.WriteLine($"{Convert.ToDecimal(number)} => {Convert.ToDecimal(roundedeNumber)}");
+
+
+
+
+            }
+        }
+    }
+}
