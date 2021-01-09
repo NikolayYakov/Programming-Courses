c240cec5db26fc97d9421fc3365e8db16311b0df
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-09 11:53:43 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Arrays/TopIntegers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+namespace TopIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+
+
+            string topInteger = string.Empty;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                int counter = 0;
+                for (int k =arr.Length-1;k>i;k--)
+                {
+                    if (arr[i] > arr[k])
+                    {
+                        counter++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    if (counter==arr.Length-i-1)
+                    {
+                        Console.Write(arr[i] + " ");
+                    }
+                    
+                  
+                  
+                }
+                if (i == arr.Length-1)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+            }
+             
+        }
+    }
+}
