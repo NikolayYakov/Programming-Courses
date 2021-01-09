411d1d3017c5834a9513e4b1a97471305a70d449
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-09 11:53:48 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Arrays/Train/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vagons = int.Parse(Console.ReadLine());
+            int[] people = new int[vagons];
+            int allPeople = 0;
+            for(int i = 0; i < vagons; i++)
+            {
+                people[i] = int.Parse(Console.ReadLine());
+                allPeople += people[i];
+            }
+            string train = string.Join(' ', people);
+            Console.WriteLine(train);
+            Console.WriteLine(allPeople);
+        }
+    }
+}
