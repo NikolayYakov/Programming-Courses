d6619966c2c95ec7af3ff3bd99a9f170e10cefb6
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-09 11:53:06 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Arrays/OddAndEvenSumDifference/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+namespace OddAndEvenSumDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersArr = Console.ReadLine()
+                             .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                             .Select(int.Parse)
+                             .ToArray();
+            int result = 0;
+            int evenSum = 0;
+            int oddSum = 0;
+            for (int i = 0; i < numbersArr.Length; i++)
+            {
+                if (numbersArr[i] % 2 == 0)
+                {
+                    evenSum += numbersArr[i];
+                }
+                else
+                {
+                    oddSum += numbersArr[i];
+                }
+            }
+            result = evenSum-oddSum;
+            Console.WriteLine(result);
+        }
+    }
+}
