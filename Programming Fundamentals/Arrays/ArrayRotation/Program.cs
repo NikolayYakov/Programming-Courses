5faea6ab440c325515a407fa01790a55aa5863a1
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-09 11:52:22 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Arrays/ArrayRotation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+namespace ArrayRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine()
+                       .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int rotates = int.Parse(Console.ReadLine());
+            for(int i = 0; i <rotates ; i++)
+            {
+                string elementToRotate = arr[0];
+                for (int j = 1; j < arr.Length; j++)
+                {
+                    string currentElement = arr[j];
+                    arr[j - 1] = currentElement;
+                }
+                arr[arr.Length - 1] = elementToRotate;
+            }
+            Console.WriteLine(string.Join(' ', arr)) ;
+        }
+        
+    }
+}
