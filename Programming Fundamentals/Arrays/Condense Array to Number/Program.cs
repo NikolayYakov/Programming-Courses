32569d27420690bf2e1a67bf272f869fe441303e
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-09 11:52:39 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Arrays/Condense Array to Number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+namespace _8._Condense_Array_to_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+          
+
+            while (arr.Length > 1)
+            {
+                 int[] condems = new int[arr.Length - 1];
+                for (int i = 0; i < condems.Length; i++)
+                {
+                    condems[i] = arr[i] + arr[i + 1];
+
+
+                }
+                arr = condems;
+            }
+            Console.WriteLine(arr[0]);
+        }
+    }
+}
