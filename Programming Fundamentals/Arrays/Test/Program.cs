4ffb9c920969018472cd6cfe9e9c2a7fa5458700
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-09 11:53:36 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Arrays/Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+            a = b;
+            b = a;
+            Console.WriteLine($"{a} {b}");
+        }
+    }
+}
