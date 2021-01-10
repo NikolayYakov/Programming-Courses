f740cad7f30a9c295fcfcbc2a5415baad0157901
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 16:08:23 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Methods/Matricks/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Matricks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Matricks(number);
+        }
+        static void Matricks(int number)
+        {
+            string text = string.Empty;
+            for(int i=1;i<=number;i++)
+            {
+                             
+                for(int k = 1; k <= number; k++)
+                {
+                    Console.Write(number + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
