c87144e8f8030b4d3766522a32dee291b0e7f552
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 16:09:07 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Methods/TopNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TopNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            TopInt(number);
+        }
+        static void TopInt(int number)
+        {
+            for (int i = 10; i < number; i++)
+            {
+                bool isOdd = false;
+                int sum = 0;
+                int old = i;
+                while (i != 0)
+                {
+                   
+                    sum += i % 10;
+                    int curr = i % 10;
+                    if (curr % 2 != 0)
+                    {
+                        isOdd = true;
+                    }
+                    i /= 10;
+                }
+                if (sum % 8 == 0 && isOdd == true)
+                {
+                    Console.WriteLine(old);
+                }
+                i = old;
+
+            }
+        }
+    }
+}
