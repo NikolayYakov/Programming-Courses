40b817fb1a6ca1433920ec73d8ecd5a00f43e0ba
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 16:07:28 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Methods/BiggerNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BiggerNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            SmallerNumber(firstNumber, secondNumber, thirdNumber);
+        }
+        static void SmallerNumber(int firstNumber,int secondNumber,int thirdNumber)
+        {
+            if (firstNumber < secondNumber && firstNumber < thirdNumber)
+            {
+                Console.WriteLine(firstNumber);
+            }
+            else if (secondNumber < firstNumber && secondNumber < thirdNumber)
+            {
+                Console.WriteLine(secondNumber);
+            }
+            else
+            {
+                Console.WriteLine(thirdNumber);
+            }
+        }
+    }
+}
