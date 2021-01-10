9d930bee1170a46c74283107f7e12d1cc69d0004
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 16:07:50 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Methods/EvenAndOdd/Program.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+
+namespace EvenAndOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int number = int.Parse(Console.ReadLine());
+            string text = number.ToString();
+            int[] arr = new int[text.Length];
+             
+           
+             
+                if (number < 0)
+                {
+                    arr = new int[text.Length - 1];
+                }
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    int result = number % 10;
+                    result = Math.Abs(result);
+                    arr[arr.Length - 1 - i] = result;
+                    number /= 10;
+                }
+                int evenSum = EvenSum(arr);
+                int oddSum = OddSum(arr);
+                int multiplayed = evenSum * oddSum;
+                Console.WriteLine(multiplayed);
+             
+        }
+        static int EvenSum(int[] arr)
+        {
+            int evenSum = 0;
+            for(int i = 0; i < arr.Length; i ++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    arr[i] = Math.Abs(arr[i]);
+                    evenSum += arr[i];
+                }
+            }
+            return evenSum;
+        }
+        static int OddSum(int[] arr)
+        {
+            int oddSum = 0;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 != 0) 
+                {
+                    arr[i] = Math.Abs(arr[i]);
+
+                    oddSum += arr[i];
+                }
+                
+            }
+            return oddSum;
+        }
+        
+    }
+}
