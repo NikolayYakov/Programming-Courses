f628dc4dbaa1b07696ccdc87afe5e07ca31655df
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 11:31:10 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Lists/RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RemoveNegativesAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                .Select(int.Parse)
+                                .ToList();
+            List<int> positive = numbers.Where(n => n > 0)
+                                 .Reverse()
+                                 .ToList();
+            if (positive.Count != 0)
+            {
+                Console.WriteLine(string.Join(" ", positive));
+            }
+            else
+            {
+                Console.WriteLine("empty");
+            }
+            
+
+        }
+    }
+}
