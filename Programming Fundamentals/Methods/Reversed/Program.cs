e5aae40a5d1d8e77fc5e946b39cae9924b72e8a7
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 16:08:57 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Methods/Reversed/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace PalindromeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+
+            while (number != "END")
+            {
+                string reversed = Arr(number);
+
+                if (number == reversed)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+                number = Console.ReadLine();
+            }
+        }
+        static string Arr(string number)
+        {
+            char[] arr = number.ToCharArray();
+            Array.Reverse(arr);
+
+
+
+            return new String(arr);
+        }
+    }
+}
+
