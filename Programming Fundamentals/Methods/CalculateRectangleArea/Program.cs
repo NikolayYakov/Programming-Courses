03e2be71ba2c8923e610e51ee5ec76f26fcefcaf
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 16:07:35 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Methods/CalculateRectangleArea/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CalculateRectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            int area = Area(width, height);
+            Console.WriteLine(area);
+        }
+        static int Area(int width,int height)
+        {
+            return width * height;
+        }
+    }
+}
