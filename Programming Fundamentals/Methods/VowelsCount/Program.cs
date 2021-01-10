d5b2762d5a5020a560c825d39074f135d5505bd4
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 16:09:12 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Methods/VowelsCount/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine().ToLower();
+            Check(text);
+
+            
+
+        }
+        static void Check(string text)
+        {
+            int count = 0;
+            for(int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == 'a' || text[i] == 'e' || text[i] == 'i' || text[i] == 'o' || text[i] == 'u')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
