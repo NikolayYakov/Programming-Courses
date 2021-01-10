8efe9d8267587b5938f39c66390f4b79f4cd0f8a
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 16:07:59 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Methods/FactorialDivided/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FactorialDivided
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            double firstFactorial = FirstFactorial(firstNumber);
+            double secondFactotial = SecondFactorial(secondNumber);
+            double result = firstFactorial / secondFactotial;
+            Console.WriteLine($"{result:f2}");
+        }
+        static double FirstFactorial(int number)
+        {
+            double factorial = 1.0;
+            for(int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+        static double SecondFactorial(int number)
+        {
+            double factorial = 1.0;
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
