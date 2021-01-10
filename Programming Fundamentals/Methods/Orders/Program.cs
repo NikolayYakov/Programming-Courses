bb0b0af2aaf679cee9a7a5e94a327213ea9fc341
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 16:08:33 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Methods/Orders/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quontity = int.Parse(Console.ReadLine());
+            TotalPrice(product, quontity);
+ 
+        }
+        static void TotalPrice(string product,int quontity)
+        {
+            decimal totalPrice = 0M;
+            switch (product)
+            {
+                case "coffee":
+                    totalPrice += 1.50M;
+                    break;
+                case "water":
+                    totalPrice += 1.00M;
+                    break;
+                case "coke":
+                    totalPrice += 1.40M;
+                    break;
+                case "snacks":
+                    totalPrice += 2.00M;
+                    break;
+            }
+            totalPrice *= quontity;
+            Console.WriteLine($"{totalPrice:f2}");
+        }
+        
+    }
+}
