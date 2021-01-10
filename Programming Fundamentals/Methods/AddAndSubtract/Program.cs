3a24e182bca71e4b0b9a52303682e76eb52beed5
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 16:07:21 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Methods/AddAndSubtract/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AddAndSubtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int result = Add(firstNumber, secondNumber);
+            int finalScore = Subtract(result, thirdNumber);
+            Console.WriteLine(finalScore);
+        }
+        static int Add(int firstNumber,int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+        static int Subtract(int added, int thirdNumber)
+        {
+            return added - thirdNumber;
+        }
+    }
+}
