dea217ffeb44fe838b01278b29be3bf8a5dfb910
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 16:07:40 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Methods/Calculations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string function = Console.ReadLine();
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            MatematicFunction(function, firstNumber, secondNumber);
+        }
+        static void MatematicFunction(string function, int firstNumber, int secondNumber)
+        {
+            int result = 0;
+            switch (function)
+            {
+                case "add":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "multiply":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "subtract":
+                    result = firstNumber - secondNumber;
+                    break;
+                case "divide":
+                    result = firstNumber / secondNumber;
+                    break;
+               
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
