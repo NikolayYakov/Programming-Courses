796996747bec4d63a2270502d5553f2d006d224e
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 16:08:28 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Methods/MiddleChar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MiddleChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            MidChar(text);
+        }
+        static void MidChar(string text)
+        {
+            if (text.Length % 2 != 0)
+            {
+                int mid = text.Length/2;
+                Console.WriteLine(text.Substring(mid, 1));
+            }
+            if (text.Length % 2 == 0)
+            {
+                int mid = text.Length / 2-1;
+                Console.WriteLine(text.Substring(mid, 2));
+            }
+        }
+    }
+}
