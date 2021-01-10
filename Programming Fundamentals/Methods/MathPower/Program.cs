01c0658372bd7accb3eea7c8787db321ad8526dd
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 16:08:19 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Methods/MathPower/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MathPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+          int resul=  finalNumber(number, power);
+            Console.WriteLine(resul);
+        }
+        static int finalNumber(int number,int power)
+        {
+            int result = number;
+            for(int i = 2; i <= power; i++)
+            {
+                result = result * number; 
+            }
+            return result;
+        }
+    }
+}
