adb80eda68cc5f579752ca3226672d129ed03323
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 11:30:34 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Lists/HouseParty/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HouseParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int listCount = int.Parse(Console.ReadLine());
+            List<string> partyList = new List<string>(listCount);
+            for(int i = 0; i < listCount; i++)
+            {
+                List<string> peopleGoing = Console.ReadLine()
+                                           .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                           .ToList();
+                if(peopleGoing[2] == "not")
+                {
+                    if (partyList.Contains(peopleGoing[0]))
+                    {
+                        partyList.Remove(peopleGoing[0]);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{peopleGoing[0]} is not in the list!");
+                    }
+                    
+                }
+                else
+                {
+                    if (partyList.Contains(peopleGoing[0]))
+                    {
+                        Console.WriteLine($"{peopleGoing[0]} is already in the list!");
+                    }
+                    else
+                    {
+                        partyList.Add(peopleGoing[0]);
+                    }
+                    
+                }
+                
+            }
+           for(int i = 0; i < partyList.Count; i++)
+            {
+                Console.WriteLine(partyList[i]);
+            }
+        }
+    }
+}
