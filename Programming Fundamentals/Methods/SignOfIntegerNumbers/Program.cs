fda78d8d85033075dfe803193deb5c60baeee817
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 16:09:03 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Methods/SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SignOfIntegerNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            NumberSign(number);
+            
+        }
+        static void NumberSign(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is zero.");
+
+            }
+        }
+    }
+}
