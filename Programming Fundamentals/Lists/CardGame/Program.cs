628f73bc1ab001048ba8a4b2ea87feb647047d43
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 11:30:17 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Lists/CardGame/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CardGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstDeck = Console.ReadLine()
+                                  .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                  .Select(int.Parse)
+                                  .ToList();
+            List<int> secondDeck = Console.ReadLine()
+                                  .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                  .Select(int.Parse)
+                                  .ToList();
+            while(Math.Min(firstDeck.Count,secondDeck.Count) > 0) 
+            {
+                if (firstDeck[0] == secondDeck[0])
+                {
+                    firstDeck.RemoveAt(0);
+                    secondDeck.RemoveAt(0);
+                    
+
+                }
+               else if (firstDeck[0] > secondDeck[0])
+                {
+                    firstDeck.Add(firstDeck[0]);
+                    firstDeck.Add(secondDeck[0]);
+                    firstDeck.RemoveAt(0);
+                    secondDeck.RemoveAt(0);
+                }
+                else
+                {
+                    secondDeck.Add(secondDeck[0]);
+                    secondDeck.Add(firstDeck[0]);
+                    firstDeck.RemoveAt(0);
+                    secondDeck.RemoveAt(0);
+                }
+            }
+            if (firstDeck.Count == 0)
+            {
+                int sum = secondDeck.Sum();
+
+                Console.WriteLine($"Second player wins! Sum: {sum}");
+            }
+            else
+            {
+                int sum = firstDeck.Sum();
+                Console.WriteLine($"First player wins! Sum: {sum}");
+            }
+        }
+    }
+}
