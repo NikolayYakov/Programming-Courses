db306d8ab33b196acad489089ea4c0459d639cb6
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 16:08:14 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Methods/MathOperations/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+            char symbol = char.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+            double result = Calculations(firstNumber, symbol, secondNumber);
+            Console.WriteLine(result);
+        }
+        static double Calculations(double firstNumber,char symbo,double secondNumber)
+        {
+            double result = 0.0;
+            switch (symbo)
+            {
+                case '+':
+                    result = firstNumber + secondNumber;
+                    break;
+                case '-':
+                    result = firstNumber - secondNumber;
+                    break;
+                case '*':
+                    result = firstNumber * secondNumber;
+                    break;
+                case '/':
+                    result = firstNumber / secondNumber;
+                    break;
+                    
+            }
+            return result;
+        }
+    }
+}
