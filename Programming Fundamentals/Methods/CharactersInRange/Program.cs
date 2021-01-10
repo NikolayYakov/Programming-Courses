15a80c79e70989921c47485a454b913ef969cd48
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 16:07:45 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Methods/CharactersInRange/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CharactersInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+            Range(firstChar, secondChar);
+        }
+        static void Range(char firstChar,char secondChar)
+        {
+            if (firstChar < secondChar)
+            {
+                for (char i = firstChar; i < secondChar; i++)
+                {
+                    if (i != firstChar)
+                    {
+                        Console.Write(i + " ");
+                    }
+
+                }
+            }
+            else
+            {
+                for (char i = secondChar; i < firstChar; i++)
+                {
+                    if (i != secondChar)
+                    {
+                        Console.Write(i + " ");
+                    }
+
+                }
+            }
+        }
+    }
+}
