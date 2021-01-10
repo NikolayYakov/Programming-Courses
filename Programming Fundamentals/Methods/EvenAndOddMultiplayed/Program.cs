05f8874fe607d17fbac75bf4729f2503b6bce910
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 16:07:55 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Methods/EvenAndOddMultiplayed/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+namespace EvenAndOddMultiplayed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersArr = Console.ReadLine()
+                               .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                               .Select(int.Parse)
+                               .ToArray();
+        }
+    }
+}
