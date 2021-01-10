0195575996289088bbc7f6d93fae977e120efa21
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 11:30:52 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Lists/ListOfProducts/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListOfProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int listSize = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>(listSize);
+            for(int i = 0; i < listSize; i++)
+            {
+                products.Add(Console.ReadLine());
+            }
+            products.Sort();
+             
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i+1}.{products[i]}");
+                
+            }
+        }
+    }
+}
