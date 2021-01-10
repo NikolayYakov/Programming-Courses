d1436528d8c953dbb91a7524e8ae4e2fb39ddc3a
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 11:30:07 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Lists/AppendArrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppendArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> arrays = Console.ReadLine()
+                                  .Split("|", StringSplitOptions.RemoveEmptyEntries)
+                                  .Where(s => !string.IsNullOrWhiteSpace(s))
+                                  .ToList();
+            string result = string.Empty;
+            for(int i = arrays.Count-1; i>=0; i--)
+            {
+                List<string> everyArr = arrays[i]
+                                  .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                  .ToList();
+
+                result += string.Join(" ", everyArr)+" ";
+            }
+
+            Console.WriteLine(result);
+             
+        }
+    }
+}
