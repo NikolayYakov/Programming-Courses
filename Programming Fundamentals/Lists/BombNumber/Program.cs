e05210cc37d08159ac54b456d13d5f4bae415999
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 11:30:12 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Lists/BombNumber/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BombNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                .Select(int.Parse)
+                                .ToList();
+            List<int> bombNumber = Console.ReadLine()
+                                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                .Select(int.Parse)
+                                .ToList();
+            int bomb = bombNumber[0];
+            int power = bombNumber[1];
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int currentNumber = numbers[i];
+                if (currentNumber == bomb)
+                {
+                    int statrIndex = i - power;
+                    int endIndex = i + power;
+                    if (statrIndex < 0)
+                    {
+                        statrIndex = 0;
+                    }
+                    if (endIndex > numbers.Count - 1)
+                    {
+                        endIndex = numbers.Count - 1;
+                    }
+                    int endIndexToRemove =endIndex-statrIndex+1;
+                    numbers.RemoveRange(statrIndex, endIndexToRemove);
+                    i = statrIndex - 1;
+                }
+            }
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
