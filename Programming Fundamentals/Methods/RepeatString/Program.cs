eade8420660c97ab9cf49e5a326ff9b0044cba30
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-10 16:08:53 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Methods/RepeatString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+namespace RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int repeat = int.Parse(Console.ReadLine());
+            string final =FinalText(text, repeat);
+            Console.WriteLine(final);
+        }
+        static string FinalText(string text,int repeat)
+        {
+            StringBuilder finalText = new StringBuilder();
+            for(int i = 1; i <= repeat; i++)
+            {
+                finalText.Append(text);
+            }
+            return finalText.ToString();
+        }
+    }
+}
