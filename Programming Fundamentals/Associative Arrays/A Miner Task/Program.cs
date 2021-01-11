a4e004d31cc5bc31b1611abef93266e3ea927ff2
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-11 13:46:19 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Associative Arrays/A Miner Task/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            Dictionary<string, int> resources = new Dictionary<string, int>();
+            while (command != "stop")
+            {
+                string resource = command;
+                int value = int.Parse(Console.ReadLine());
+                if (resources.ContainsKey(resource))
+                {
+                    resources[resource]+=value;
+                }
+                else
+                {
+                    resources.Add(resource, value);
+                }
+                command = Console.ReadLine();
+            }
+            foreach ( var resource in resources)
+            {
+                Console.WriteLine($"{resource.Key} -> {resource.Value}");
+            }
+        }
+    }
+}
