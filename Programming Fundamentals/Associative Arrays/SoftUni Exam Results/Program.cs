266593d525c13197847af792263c7494e4c2f41e
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-11 13:46:59 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Associative Arrays/SoftUni Exam Results/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni_Exam_Results
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> students = new Dictionary<string, int>();
+            Dictionary<string, int> languages = new Dictionary<string, int>();
+            string[] command = Console.ReadLine()
+                                      .Split("-", StringSplitOptions.RemoveEmptyEntries);
+            
+            while(command[0]!= "exam finished")
+            {
+                if (command.Contains("banned")==false)
+                {
+                    string name = command[0];
+                    int points = int.Parse(command[2]);
+                    string language = command[1];
+                    if (students.ContainsKey(name) == false)
+                    {
+                        students.Add(name, points);
+                    }
+                    else
+                    {
+                        if (students[name] < points)
+                        {
+                            students[name] = points;
+                        }
+                    }
+                   
+                    if (languages.ContainsKey(language))
+                    {
+                        languages[language]++;
+                    }
+                    else
+                    {
+                        languages.Add(language, 1);
+                    }
+ 
+                }
+                else
+                {
+                    string name = command[0];
+                    if (students.ContainsKey(name))
+                    {
+                        students.Remove(name);
+                    }
+                }
+                command = Console.ReadLine()
+                                  .Split("-", StringSplitOptions.RemoveEmptyEntries);
+
+            }
+            Console.WriteLine("Results:");
+            foreach (var student in students.OrderByDescending(x=>x.Value).ThenBy(x=>x.Key))
+            {
+                Console.WriteLine($"{student.Key} | {student.Value}");
+            }
+            Console.WriteLine("Submissions:");
+            foreach (var language in languages.OrderByDescending(x=>x.Value).ThenBy(x=>x.Key))
+            {
+                Console.WriteLine($"{language.Key} - {language.Value}");
+            }
+             
+        }
+    }
+}
