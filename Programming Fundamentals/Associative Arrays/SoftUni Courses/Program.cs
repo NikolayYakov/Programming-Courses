fec9224fd90b2bff4a3ae5abb7a911d4e9786d81
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-11 13:46:55 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Associative Arrays/SoftUni Courses/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Courses
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+            string[] command = Console.ReadLine()
+                                      .Split(" : ", StringSplitOptions.RemoveEmptyEntries);
+            while (command[0] != "end")
+            {
+                string course = command[0];
+                string student = command[1];
+                if (courses.ContainsKey(course))
+                {
+                    courses[course].Add(student);
+
+                }
+                else
+                {
+                    courses.Add(course, new List<string>());
+                    courses[course].Add(student);
+                }
+                command = Console.ReadLine()
+                                 .Split(" : ", StringSplitOptions.RemoveEmptyEntries);
+            }
+            foreach (var course in courses.OrderByDescending(s => s.Value.Count))
+            {
+                Console.WriteLine($"{course.Key}: {course.Value.Count}");
+                foreach (var student in course.Value.OrderBy(x => x))
+                {
+                    Console.WriteLine($"-- {student}");
+                }
+
+            }
+
+        }
+    }
+}
+
