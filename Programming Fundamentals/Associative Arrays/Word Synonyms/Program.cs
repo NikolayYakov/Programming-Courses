1b71810c297f7c4dda52dee89fef2120ce0358c0
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-11 13:47:18 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Associative Arrays/Word Synonyms/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
+            int wordsCount = int.Parse(Console.ReadLine());
+            for(int i = 0; i < wordsCount; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+                if (words.ContainsKey(word))
+                {
+                    words[word].Add(synonym);
+                }
+                else
+                {
+                    words.Add(word, new List<string>());
+                    words[word].Add(synonym);
+                }
+            }
+            foreach (var word in words)
+            {
+                Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
+            }
+
+        }
+    }
+}
