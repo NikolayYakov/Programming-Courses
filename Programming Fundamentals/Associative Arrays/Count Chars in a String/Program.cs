d95734e75955e5997431290ffb36a9e3516f806d
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-11 13:46:27 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Associative Arrays/Count Chars in a String/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+             
+            char[] chars = word.ToCharArray();
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+            foreach (var symbol in chars)
+            {
+                if (symbol != ' ')
+                {
+                    if (dictionary.ContainsKey(symbol))
+                    {
+                        dictionary[symbol]++;
+                    }
+                    else
+                    {
+                        dictionary.Add(symbol, 1);
+                    }
+                }
+            }
+            foreach (var symbols in dictionary)
+            {
+                Console.WriteLine($"{symbols.Key} -> {symbols.Value}");
+            }
+        }
+    }
+}
