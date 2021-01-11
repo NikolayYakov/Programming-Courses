a5c961294f25c3e9bc4846f7ab92a50f8b6d7d26
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-11 13:46:14 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Associative Arrays/8. Company Users/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8._Company_Users
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, List<string>> companies = new SortedDictionary<string, List<string>>();
+            string[] command = Console.ReadLine()
+                                      .Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+            while (command[0] != "End")
+            {
+                string company = command[0];
+                string id = command[1];
+                if (companies.ContainsKey(company))
+                {
+                    bool isIdContained = companies[company].Contains(id);
+                    if (isIdContained == false)
+                    {
+                        companies[company].Add(id);
+                    }
+                }
+                else
+                {
+                    companies.Add(company, new List<string>());
+                    companies[company].Add(id);
+                }
+
+                command = Console.ReadLine()
+                                  .Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+            }
+            foreach (var company in companies)
+            {
+                Console.WriteLine(company.Key);
+                for(int i = 0; i < company.Value.Count; i++)
+                {
+                    Console.WriteLine($"-- {company.Value[i]}");
+                }
+            }
+        }
+    }
+}
