73d0c5de5df1c0a1dd65fd9eaf4e07d648df09ff
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-11 13:46:31 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Associative Arrays/Count Real Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                                      .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                      .Select(double.Parse)
+                                      .ToArray();
+            var dictionary = new SortedDictionary<double, int>();
+            foreach (double number in numbers)
+            {
+                if (dictionary.ContainsKey(number))
+                {
+                    dictionary[number]++;
+                }
+                else
+                {
+                    dictionary.Add(number, 1);
+                }
+            }
+            foreach (var number in dictionary)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
