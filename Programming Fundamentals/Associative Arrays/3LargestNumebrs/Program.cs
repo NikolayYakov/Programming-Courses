47b92c89058a07e57ac711ca04a95e0466cfe90c
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-11 13:46:09 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Associative Arrays/3LargestNumebrs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3LargestNumebrs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                                       .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                       .Select(int.Parse)
+                                       .ToList();
+            List<int> maxList = new List<int>(3);
+            for(int i = 0; i < 3; i++)
+            {
+                if (numbers.Count <= 0)
+                {
+                    break;
+                }
+                int max = numbers.Max();
+                maxList.Add(max);
+                numbers.Remove(max);
+
+            }
+            Console.WriteLine(string.Join(" ", maxList));
+        }
+    }
+}
