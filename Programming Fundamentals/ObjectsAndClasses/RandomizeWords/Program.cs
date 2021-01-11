c847fd85902deafee9ff75a0b93d84538902cd55
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-11 13:47:49 +0200

    Create Program.cs

---
+++ Programming Fundamentals/ObjectsAndClasses/RandomizeWords/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RandomizeWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrWords = Console.ReadLine()
+                                      .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Random random = new Random();
+            for(int i = 0; i < arrWords.Length; i++)
+            {
+                int index = random.Next(0, arrWords.Length);
+                string word = arrWords[i];
+                arrWords[i] =arrWords[index];
+                arrWords[index] = word;
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, arrWords));
+        }
+    }
+}
