391ff3db235bfdd1c35e64bcc51ff1255fb4a464
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-11 13:47:13 +0200

    Create Program.cs

---
+++ Programming Fundamentals/Associative Arrays/Word Filter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Word_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Where(x => x.Length % 2 == 0)
+                                   .ToArray();
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+
+             
+        }
+    }
+}
