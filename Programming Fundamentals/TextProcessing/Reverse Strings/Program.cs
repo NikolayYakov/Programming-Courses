94d8ee70c052a14a098c30dc10aee4958216b593
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-12 13:22:09 +0200

    Create Program.cs

---
+++ Programming Fundamentals/TextProcessing/Reverse Strings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> words = new Dictionary<string, string>();
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                string word = command;
+                char[] reverse = word.ToCharArray();
+                Array.Reverse(reverse);
+                string reversed = new string(reverse);
+                words.Add(word, reversed);
+                command = Console.ReadLine();
+            }
+            foreach (var word in words)
+            {
+                Console.WriteLine($"{word.Key} = {word.Value}");
+            }
+        }
+    }
+}
