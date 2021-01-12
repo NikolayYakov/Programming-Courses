3323563494f61633fb206526d9f442d174a083f1
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-12 13:20:42 +0200

    Create Program.cs

---
+++ Programming Fundamentals/RegularExprexion/NameCheck/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace NameCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            var reg = new Regex(pattern);
+            string text = Console.ReadLine();
+            var matches = reg.Matches(text);
+            Console.WriteLine(string.Join(" ", matches));
+        }
+    }
+}
