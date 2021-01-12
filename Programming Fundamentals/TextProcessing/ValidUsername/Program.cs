be67fe0958905221feda85d29cb85d7ce2382947
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-12 13:22:39 +0200

    Create Program.cs

---
+++ Programming Fundamentals/TextProcessing/ValidUsername/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ValidUsername
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine()
+                                        .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            foreach (var username in usernames)
+            {
+                bool isValid = false;
+                char[] symbols = username.ToCharArray();
+                foreach (var symbol in symbols)
+                {
+                    if (username.Length >= 3 && username.Length <= 16)
+                    {
+                        if (char.IsDigit(symbol))
+                        {
+                            isValid = true;
+                        }
+                        else if (char.IsLetter(symbol))
+                        {
+                            isValid = true;
+                        }
+                        else if (symbol == '-')
+                        {
+                            isValid = true;
+                        }
+                        else if (symbol == '_')
+                        {
+                            isValid = true;
+                        }
+                        else
+                        {
+                            isValid = false;
+                        }
+                    }
+                    
+                }
+                if (isValid == true)
+                {
+                    Console.WriteLine(username);
+                }
+
+            }
+        }
+    }
+}
