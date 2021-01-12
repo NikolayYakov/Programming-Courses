1f87887e4fa0407da7e8276b2c91c4efc86c9917
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-12 13:22:20 +0200

    Create Program.cs

---
+++ Programming Fundamentals/TextProcessing/Substring/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string removeWord = Console.ReadLine().ToLower();
+            string text = Console.ReadLine();
+            int index = text.IndexOf(removeWord);
+            while (index != -1)
+            {
+                 text = text.Remove(index, removeWord.Length);
+
+                index = text.IndexOf(removeWord);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
