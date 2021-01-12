64468648c61a879354abec1219c0ebc52a079846
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-12 13:20:31 +0200

    Create Program.cs

---
+++ Programming Fundamentals/RegularExprexion/Match Dates/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Match_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b(?<day>\d{2})([.\/-])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+            string date = Console.ReadLine();
+            var matches = Regex.Matches(date, pattern);
+            foreach (Match item in matches)
+            {
+                Console.WriteLine($"Day: {item.Groups["day"]}, Month: {item.Groups["month"]}, Year: {item.Groups["year"]}");
+            }
+        }
+    }
+}
