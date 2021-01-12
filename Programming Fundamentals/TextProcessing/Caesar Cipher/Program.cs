300839c362c35d9e81ff0ae3702f373fb829458e
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-12 13:21:11 +0200

    Create Program.cs

---
+++ Programming Fundamentals/TextProcessing/Caesar Cipher/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+namespace Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            char[] arr = text.ToCharArray();
+            for(int i = 0; i < arr.Length; i++)
+            {
+                int value = (int)arr[i];
+                value += 3;
+                arr[i] = (char)value;
+            }
+            Console.WriteLine(string.Join("", arr));
+        }
+    }
+}
