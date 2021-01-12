003911a02bd9c11330c4fe42df31b2738a2cadd9
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-12 13:20:37 +0200

    Create Program.cs

---
+++ Programming Fundamentals/RegularExprexion/Match Phone Number/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"([+359]{4})([\s-])[2]\2([0-9]{3})\2([0-9]{4}\b)";
+            string phone = Console.ReadLine();
+            var matches = Regex.Matches(phone, pattern);
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
+}
