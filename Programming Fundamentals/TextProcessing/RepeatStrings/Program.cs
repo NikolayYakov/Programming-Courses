ac725279f588cff8a19f2b594f0d71b51536e4b8
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-12 13:21:54 +0200

    Create Program.cs

---
+++ Programming Fundamentals/TextProcessing/RepeatStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+namespace RepeatStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            StringBuilder combinerd = new StringBuilder();
+            foreach (var word in words)
+            {
+                for(int i = 0; i < word.Length; i++)
+                {
+                    combinerd.Append(word);
+                }
+            }
+            Console.WriteLine(combinerd);
+        }
+    }
+}
