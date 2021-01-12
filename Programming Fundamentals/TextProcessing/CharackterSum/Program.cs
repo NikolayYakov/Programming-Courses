40475b08b668697e4d5472c22961306307ee79a0
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-12 13:21:16 +0200

    Create Program.cs

---
+++ Programming Fundamentals/TextProcessing/CharackterSum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace CharackterSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int value = Multiplied(words[0], words[1]);
+            Console.WriteLine(value);
+        }
+        static int Multiplied(string firstString,string secondString)
+        {
+            char[] firstStringChars = firstString.ToCharArray();
+            char[] secondStringChars = secondString.ToCharArray();
+            int value = 0; ;
+            for(int i = 0; i < Math.Max(firstStringChars.Length, secondStringChars.Length); i++)
+            {
+                int first = 0;
+                int second = 0;
+                if (i < Math.Min(firstStringChars.Length, secondStringChars.Length))
+                {
+                    first = firstStringChars[i];
+                    second = secondStringChars[i];
+                    first *= second;
+                    value += first;
+                }
+                if (i >= firstStringChars.Length)
+                {
+                    second = secondStringChars[i];
+                    value += second;
+                }
+                if (i >= secondStringChars.Length)
+                {
+                    first = firstStringChars[i];
+                    value += first;
+                }
+                
+            }
+            return value;
+
+        }
+    }
+}
