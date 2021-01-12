76361b85d42559bf6ee326f2cbadb93db5a13b5a
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-12 13:21:26 +0200

    Create Program.cs

---
+++ Programming Fundamentals/TextProcessing/Extract File/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] file = Console.ReadLine()
+                                   .Split("\\", StringSplitOptions.RemoveEmptyEntries);
+            string last = file[file.Length - 1];
+            string[] name = last.Split(".", StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine($"File name: {name[0]}");
+            Console.WriteLine($"File extension: {name[1]}");
+        }
+    }
+}
