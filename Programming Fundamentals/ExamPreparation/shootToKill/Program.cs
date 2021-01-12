ac2c4ef7e6bb11769227dab3b3183e4ce1e41ab8
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-12 13:19:33 +0200

    Create Program.cs

---
+++ Programming Fundamentals/ExamPreparation/shootToKill/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace shootToKill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] targets = Console.ReadLine()
+                                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray();
+            int shotTargets = 0;
+            string command = Console.ReadLine();
+            while (command.ToLower() != "end")
+            {
+                int index = int.Parse(command);
+                if (index >= 0 && index < targets.Length && targets[index] != -1)
+                {
+                    shotTargets += 1;
+                    
+                    for(int i = 0; i < targets.Length; i++)
+                    {
+                        if (targets[i] > targets[index] && targets[i] != -1&& i != index)
+                        {
+                            targets[i] -= targets[index];
+                        }
+                        else if (targets[i] <= targets[index] && targets[i] != -1 && i!=index)
+                        {
+                            targets[i] += targets[index];
+                        }
+                    }
+                    targets[index] = -1;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Shot targets: {shotTargets} -> {string.Join(" ", targets)}");
+        }
+    }
+}
