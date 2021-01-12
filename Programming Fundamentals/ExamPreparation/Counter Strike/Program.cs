464acd8d7f669d0a2d07d4321db21b7ea8f15a6c
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-12 13:19:15 +0200

    Create Program.cs

---
+++ Programming Fundamentals/ExamPreparation/Counter Strike/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Counter_Strike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+            int wins = 0;
+            string distance = Console.ReadLine();
+            bool flag = true;
+            while(distance!= "End of battle")
+            {
+                int diff = 0;
+                int realDistance = int.Parse(distance);
+                diff = energy - realDistance;
+
+                if (diff>=0)
+                {
+                    energy -= realDistance;
+                    wins += 1;
+                }
+                if (wins % 3 == 0)
+                {
+                    energy += wins;
+                }
+
+                if (diff < 0)
+                {
+                    flag = false;
+                    
+                    Console.WriteLine($"Not enough energy! Game ends with {wins} won battles and {energy} energy");
+                    break;
+                }
+                
+                distance = Console.ReadLine();
+            }
+            if (flag == true)
+            {
+                Console.WriteLine($"Won battles: {wins}. Energy left: {energy}");
+            }
+        }
+    }
+}
