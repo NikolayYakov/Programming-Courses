ba4b351dc8ed4447e059be9f5d5d686e89b48312
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-15 16:54:39 +0200

    Create Program.cs

---
+++ Csharp Advanced/Stacks and Queues/Reverse Strings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> reversedString = new Stack<char>();
+            for(int i = 0; i < input.Length; i++)
+            {
+                reversedString.Push(input[i]);
+
+            }
+            Console.WriteLine(string.Join("", reversedString.ToArray()));
+        }
+    }
+}
