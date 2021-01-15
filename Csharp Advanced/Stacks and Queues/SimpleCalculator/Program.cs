fe0cdc0a004e110324d2f788abded463772f42bf
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-15 16:54:46 +0200

    Create Program.cs

---
+++ Csharp Advanced/Stacks and Queues/SimpleCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            Stack<string> problem = new Stack<string>(input.Reverse());
+             
+            while (problem.Count>1)
+            {
+                int result = int.Parse(problem.Pop());
+                string sign = problem.Pop();
+                if (sign == "-")
+                {
+                    result -= int.Parse(problem.Pop());
+                }
+                else
+                {
+                    result += int.Parse(problem.Pop());
+                }
+                problem.Push(result.ToString());
+            }
+            int finaleResult = int.Parse(problem.Pop());
+            Console.WriteLine(finaleResult);
+           
+        }
+    }
+}
