603156b92f66bc922f52aba11ecc2cfece77119b
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-15 16:55:02 +0200

    Create Program.cs

---
+++ Csharp Advanced/Stacks and Queues/Supermarket/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            Queue<string> people = new Queue<string>();
+            while (command != "End")
+            {
+                if (command == "Paid")
+                {
+                    while (people.Count > 0)
+                    {
+                        Console.WriteLine(people.Dequeue());
+                    }
+                }
+                else
+                {
+                    people.Enqueue(command);
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"{people.Count()} people remaining.");
+        }
+    }
+}
