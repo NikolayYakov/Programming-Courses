e9a51420b56b86e5f824715699b4cc6965077072
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-15 16:54:25 +0200

    Create Program.cs

---
+++ Csharp Advanced/Stacks and Queues/MatchingBrackets/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string problem = Console.ReadLine();
+            Stack<int> parts = new Stack<int>();
+            for(int i = 0; i < problem.Length; i++)
+            {
+                if (problem[i] == '(')
+                {
+                    
+                    parts.Push(i);
+                }
+                if (problem[i] == ')')
+                {
+                     
+                    int startIndex = parts.Pop();
+                    string part = problem.Substring(startIndex, i - startIndex + 1);
+                    Console.WriteLine(part);
+                }
+            }
+        }
+    }
+}
