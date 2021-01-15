143f906bc0d15e5f0dbd2f0c4b40958ec395b8c8
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-15 16:55:12 +0200

    Create Program.cs

---
+++ Csharp Advanced/Stacks and Queues/TraficJam/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TraficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+            Queue<string> traffic = new Queue<string>();
+            int count = 0;
+            while(command!= "end")
+            {
+                if (command == "green")
+                {
+                    for(int i = 0; i < number; i++)
+                    {
+                        if (traffic.Count == 0)
+                        {
+                            break;
+                        }
+                        Console.WriteLine($"{traffic.Dequeue()} passed!");
+                        count++;
+                        
+                    }
+                }
+                else
+                {
+                    traffic.Enqueue(command);
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"{count} cars passed the crossroads.");
+        }
+    }
+}
