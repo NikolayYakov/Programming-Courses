ca6b25f5d3cbe1e66f960fa7388ba20a8833659d
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-15 16:54:21 +0200

    Create Program.cs

---
+++ Csharp Advanced/Stacks and Queues/KeysRevolver v2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KeysRevolver_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bulletPrice = int.Parse(Console.ReadLine());
+            int bulletBarrel = int.Parse(Console.ReadLine());
+            int bulletFired = 0;
+            int[] bullets = Console.ReadLine()
+                                   .Split()
+                                   .Select(int.Parse)
+                                   .ToArray();
+            int[] locks = Console.ReadLine()
+                                   .Split()
+                                   .Select(int.Parse)
+                                   .ToArray();
+            Stack<int> bulletsStack = new Stack<int>(bullets);
+            Queue<int> locksQueue = new Queue<int>(locks);
+            int inteligenceValue = int.Parse(Console.ReadLine());
+            int bulletCount = 0;
+            while (bulletsStack.Count > 0 && locksQueue.Count > 0)
+            {
+              
+                int bullet = bulletsStack.Pop();
+                bulletCount++;
+                bulletFired++;
+                int currentLock = locksQueue.Peek();
+                if (currentLock >= bullet)
+                {
+                    Console.WriteLine("Bang!");
+                    locksQueue.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+                if (bulletBarrel == bulletFired&&bulletsStack.Count>0)
+                {
+                    Console.WriteLine("Reloading!");
+                    bulletFired = 0;
+                }
+            }
+            if (locksQueue.Count == 0)
+            {
+                int price = inteligenceValue - bulletCount * bulletPrice;
+                int bulletLeft = bullets.Length - bulletCount;
+                Console.WriteLine($"{bulletLeft} bullets left. Earned ${price}");
+            }
+            else
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locksQueue.Count}");
+            }
+        }
+    }
+}
