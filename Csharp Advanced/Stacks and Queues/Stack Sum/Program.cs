4dd60a917ca2f4fddfddfab504c6ec8b9188b76e
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-15 16:54:57 +0200

    Create Program.cs

---
+++ Csharp Advanced/Stacks and Queues/Stack Sum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                                   .Split()
+                                   .Select(int.Parse)
+                                   .ToArray();
+            string[] command = Console.ReadLine().ToLower().Split();
+            Stack<int> numbersStack = new Stack<int>(numbers);
+            
+            while (command[0] != "end")
+            {
+                if (command[0] == "add")
+                {
+                    int firstNumber = int.Parse(command[1]);
+                    int secondNumber = int.Parse(command[2]);
+                    numbersStack.Push(firstNumber);
+                    numbersStack.Push(secondNumber);
+                }
+                else
+                {
+                    int count = int.Parse(command[1]);
+                    if (count < numbersStack.Count) 
+                    {
+                        for (int i = 0; i < count; i++)
+                        {
+                            numbersStack.Pop();
+
+                        }
+                    }
+                   
+                }
+                command = Console.ReadLine().ToLower().Split();
+            }
+            Console.WriteLine($"Sum: {numbersStack.Sum()}");
+        }
+    }
+}
