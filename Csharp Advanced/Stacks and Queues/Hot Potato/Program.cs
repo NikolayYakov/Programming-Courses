4f7d630e7a1f0419333fcf87b8421ab213827f9d
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-15 16:54:16 +0200

    Create Program.cs

---
+++ Csharp Advanced/Stacks and Queues/Hot Potato/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kids = Console.ReadLine().Split();
+            Queue<string> hotPatato = new Queue<string>(kids);
+            int throws = int.Parse(Console.ReadLine());
+            int count = 1;
+            while (hotPatato.Count > 1)
+            {
+
+                if (count == throws)
+                {
+                    Console.WriteLine($"Removed {hotPatato.Dequeue()}");
+                    count = 1;
+ 
+                }
+                else
+                {
+                    count++;
+                    string kid = hotPatato.Dequeue();
+                    hotPatato.Enqueue(kid);
+                }
+            }
+            Console.WriteLine($"Last is {hotPatato.Dequeue()}");
+        }
+    }
+}
