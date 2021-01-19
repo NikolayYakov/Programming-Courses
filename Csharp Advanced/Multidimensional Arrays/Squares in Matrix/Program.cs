ec482d12b7fcc0e9846bcab74d509ebedbfddd7b
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-19 17:24:51 +0200

    Create Program.cs

---
+++ Csharp Advanced/Multidimensional Arrays/Squares in Matrix/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            char[,] matrix = new char[size[0], size[1]];
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                char[] line = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[rows, col] = line[col];
+
+                }
+            }
+            int count = 0;
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1] && matrix[row, col] == matrix[row + 1, col] && matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
