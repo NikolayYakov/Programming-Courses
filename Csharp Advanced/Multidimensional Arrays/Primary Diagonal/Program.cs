0271f97d8a90cc9c4d46f91910f082df06728cad
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-19 17:24:37 +0200

    Create Program.cs

---
+++ Csharp Advanced/Multidimensional Arrays/Primary Diagonal/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n,n];
+            int diagonalSum = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] line = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = line[col];
+                }
+            }
+            for(int i = 0; i < matrix.GetLength(1); i++)
+            {
+                diagonalSum += matrix[i,i];
+            }
+            Console.WriteLine(diagonalSum);
+            
+        }
+    }
+}
