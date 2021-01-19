118a4a5ec9cf06218e87124cfaee91a6d7868024
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-19 17:23:51 +0200

    Create Program.cs

---
+++ Csharp Advanced/Multidimensional Arrays/Maximal Sum/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Maximal_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[,] matrix = new int[size[0], size[1]];
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                int[] row = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[rows, col] = row[col];
+
+                }
+            }
+            int maxSum = int.MinValue;
+            int maxRow = 0;
+            int maxCol = 0;
+            for (int row = 0; row < matrix.GetLength(0) - 2; row++)
+            {
+                int sum = int.MinValue;
+                for (int col = 0; col < matrix.GetLength(1) - 2; col++)
+                {
+                    sum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2] + matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2] + matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        maxRow = row;
+                        maxCol = col;
+                    }
+                }
+            }
+            Console.WriteLine($"Sum = {maxSum}");
+            Console.WriteLine($"{matrix[maxRow, maxCol]} {matrix[maxRow, maxCol + 1]} {matrix[maxRow,maxCol+2]}");
+            Console.WriteLine($"{matrix[maxRow+1, maxCol]} {matrix[maxRow+1, maxCol + 1]} {matrix[maxRow+1, maxCol + 2]}");
+            Console.WriteLine($"{matrix[maxRow+2, maxCol]} {matrix[maxRow+2, maxCol + 1]} {matrix[maxRow+2, maxCol + 2]}");
+        }
+    }
+}
