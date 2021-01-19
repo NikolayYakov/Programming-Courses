3d8f5ba330a9e3f1e48230028740e738e07c4174
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-01-19 17:23:24 +0200

    Create Program.cs

---
+++ Csharp Advanced/Multidimensional Arrays/Equal Elements/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Equal_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            char[,] matrix = new char[size[0], size[1]];
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                char[] line = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[rows, col] = line[col];
+
+                }
+            }
+            int count = 0;
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    char currentElement = matrix[row, col];
+                    if (currentElement == matrix[row, col + 1] && currentElement == matrix[row + 1, col] && currentElement == matrix[row + 1, col + 1])
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
