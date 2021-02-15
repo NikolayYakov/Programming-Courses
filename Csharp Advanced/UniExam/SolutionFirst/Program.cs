ce95375434a6171c3f59e7aab2206e2e1141f57e
Nikolay Yakov <46966541+NikolayYakov@users.noreply.github.com>
2021-02-15 18:58:03 +0200

    UniExam

---
+++ Csharp Advanced/UniExam/SolutionFirst/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+
+namespace SolutionFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+            int[] arrNumber = ReadArrayFromFile(path);
+            Console.WriteLine($"Toва е масива {string.Join(", ", arrNumber)}");
+            int[] reversed = ReverseInplace(arrNumber);
+            int count = MostCommonCount(arrNumber);
+            Console.WriteLine($"Това е обътнатия масив {string.Join(", ", reversed)}");
+            Console.WriteLine($"Това е броя на най-често повтарящия се елемент {count}");
+        }
+        static int[] ReadArrayFromFile(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+
+                while (!reader.EndOfStream)
+                {
+                    string[] arr = reader.ReadLine().Split(", ");
+                    int[] numberArr = new int[arr.Length];
+                    for (int i = 0; i < numberArr.Length; i++)
+                    {
+                        int number = int.Parse(arr[i]);
+                        numberArr[i] = number;
+                    }
+                    return numberArr;
+                }
+                return null;
+            }
+        }
+        static int[] ReverseInplace(int[] arr)
+        {
+
+            int firstIndex = 0;
+            int secondIndex = arr.Length - 1;
+            while (firstIndex < secondIndex)
+            {
+                int element = arr[firstIndex];
+                arr[firstIndex] = arr[secondIndex];
+                arr[secondIndex] = element;
+                firstIndex++;
+                secondIndex--;
+            }
+            return arr;
+        }
+        static int MostCommonCount(int[] arrCommon)
+        {
+            int mostCommonElement = 0;
+            int finalResult = 1;
+            for (int i = 0; i < arrCommon.Length; i++)
+            {
+                int count = 1;
+                mostCommonElement = arrCommon[i];
+                for (int j = 0; j < arrCommon.Length; j++)
+                {
+                    if (i != j && arrCommon[i] == arrCommon[j])
+                    {
+                        count++;
+                        if (count > finalResult)
+                        {
+                            finalResult = count;
+                        }
+                    }
+                }
+            }
+            return finalResult;
+
+        }
+    }
+}
---
+++ Csharp Advanced/UniExam/SolutionSecond/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace SolutionSecond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            string text = string.Empty;
+            while (line != string.Empty)
+            {
+                text += line + " ";
+                line = Console.ReadLine();
+            }
+            text = text.Trim();
+            int count = CountWords(text);
+            Console.WriteLine($"Това е броя на думите {count}");
+            int numberCount = CountDigits(text);
+            Console.WriteLine($"Това е броя на цифрите {numberCount}");
+            LettersHistogram(text);
+
+        }
+        static int CountWords(string text)
+        {
+            int count = 0;
+            for(int i = 0; i < text.Length; i++)
+            {
+                if (char.IsLetter(text[i]))
+                {
+                    while (char.IsLetter(text[i]))
+                    {
+                        i++;
+                        if (i >= text.Length)
+                        {
+                            break;
+                        }
+                    }
+                    count++;
+                }
+               
+            }
+            return count;
+        }
+        static int CountDigits(string text)
+        {
+            int Numbercount = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsDigit(text[i]))
+                {
+                    while (char.IsDigit(text[i]))
+                    {
+                        i++;
+                        if (i >= text.Length)
+                        {
+                            break;
+                        }
+                    }
+                    i--;
+                    Numbercount++;
+                }
+                
+            }
+            return Numbercount;
+        }
+        static void LettersHistogram(string text)
+        {
+            int[] count = new int[26];
+            for(int i = 0; i < text.Length; i++)
+            {
+                if (char.IsLetter(text[i]))
+                {
+                    char symbol = char.ToLower(text[i]);
+                    count[symbol - 'a']++;
+                }
+               
+            }
+            for(int i = 0;i < count.Length; i++)
+            {
+                Console.WriteLine($"Броя на буквата {(char)('a' + i)} се среща {count[i]}");
+            }
+        }
+    }
+}
