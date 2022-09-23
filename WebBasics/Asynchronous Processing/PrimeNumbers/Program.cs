9ae4b733920b1779913772185a1ad118a80ec02b
Nikolay Yakov <n.n.yakov1@gmail.com>
2022-09-23 19:49:29 +0300

    async

---
+++ WebBasics/Asynchronous Processing/PrimeNumbers/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+
+    
+ Stopwatch sw = Stopwatch.StartNew();
+//Task.Run(FindThePrimeNumber) ;
+Thread[] threads = new Thread[4];
+threads[0] = new Thread(() => FindThePrimeNumber(1, 2_500_000));
+threads[1] = new Thread(() => FindThePrimeNumber(2_500_001, 5_000_000));
+threads[2] = new Thread(() => FindThePrimeNumber(5_000_001, 7_500_000));
+threads[3] = new Thread(() => FindThePrimeNumber(7_500_001, 10_000_000));
+
+foreach (var thread in threads)
+{
+    thread.Start();
+}
+
+foreach (var thread in threads)
+{
+    thread.Join();
+}
+
+sw.Stop();
+Console.WriteLine(sw.Elapsed);
+Console.WriteLine(count);
+
+while (true)
+{
+    
+    var input = Console.ReadLine();
+    Console.WriteLine(input.ToUpper());
+}
+
+
+static void FindThePrimeNumber(int start, int size)
+{
+    
+    //int n = 10000000;
+    int count = 0;
+    for (int i = start; i < size; i++)
+    {
+        bool isPrime = true;
+        for (int j = 2; j <= Math.Sqrt(i); j++)
+        {
+            if (i % j == 0)
+            {
+                isPrime = false;
+                break;
+            }
+
+        }
+        if (isPrime)
+        {
+            count++;
+        }
+
+    }
+   
+    
+    
+}
+
+ 
+
+
+
+
---
+++ WebBasics/Asynchronous Processing/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ShowImage(image1, "https://http.cat/100");
+            ShowImage(image2, "https://http.cat/101");
+            ShowImage(image3, "https://http.cat/102");
+            ShowImage(image4, "https://http.cat/200");
+            ShowImage(image5, "https://http.cat/201");
+            ShowImage(image6, "https://http.cat/202");
+            
+
+        }
+
+        private void ShowImage(Image image, string url)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var result = httpClient.GetAsync(url).GetAwaiter().GetResult();
+                byte[] imageBytes = result.Content.ReadAsByteArrayAsync().GetAwaiter().GetResult();
+                image.Source = LoadImage(imageBytes);
+            }
+           
+        }
+        private static BitmapImage LoadImage(byte[] imageData)
+        {
+            if (imageData == null || imageData.Length == 0) return null;
+            var image = new BitmapImage();
+            using (var mem = new MemoryStream(imageData))
+            {
+                mem.Position = 0;
+                image.BeginInit();
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = null;
+                image.StreamSource = mem;
+                image.EndInit();
+            }
+            image.Freeze();
+            return image;
+        }
+    }
+}
